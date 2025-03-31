9e5bfa951f322170e04afea48a84cb49acbc642e
miing_gaeng <pem4134@naver.com>
2025-03-31 19:49:53 +0900

    setting: app 페이지 라우팅 설정

---
+++ src/app/(main-contents)/rank/teams/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TeamRankPage = () => {
+  return <div>TeamRankPage</div>;
+};
+
+export default TeamRankPage;
---
+++ src/app/(main-contents)/rank/users/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserRankPage = () => {
+  return <div>UserRankPage</div>;
+};
+
+export default UserRankPage;
