bc936f8b29b504a26f4b03bb1bb7d40199180cd8
verdantgreeny <verdantgreeny@gmail.com>
2025-04-03 00:30:10 +0900

    Cleanup: fetchGetAllHabits에 주석 추가 #12

--- src/lib/services/habit-actions.services.ts
+++ src/lib/services/habit-actions.services.ts
@@ -3,6 +3,11 @@
 import { Habit } from '@prisma/client';
 import { prisma } from '../prisma';
 
+/**
+ * 사용자의 모든 Habit 목록을 조회하는 함수
+ * @param {string} userId - 조회할 사용자의 ID
+ * @returns {Promise<Habit[]>} - 사용자의 Habit 목록
+ */
 export const fetchGetAllHabits = async (userId: string): Promise<Habit[]> => {
   return await prisma.habit.findMany({
     where: { userId },
