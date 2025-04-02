3be3a5965496f28908785a37ac7b3263bfcbfcac
Handsol <imhandsol@gmail.com>
2025-04-02 23:25:27 +0900

    Feat: 단일 사용자 프로필 Patch 로직 추가 #15

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -53,3 +53,64 @@ export const GET = async (request: Request, { params }: RouteParams) => {
     );
   }
 };
+
+/**
+ * Profile 수정
+ * @param {Request} - PATCH 요청
+ * @returns {Promise<NextResponse>} - 수정된 User 정보 또는 에러
+ * @throws {Error} 데이터베이스 업데이트 실패했을 때
+ * @description
+ * - 인증된 사용자가 자신의 Profile 수정
+ * - 유효성 검사를 통해 데이터 형식과 길이를 확인
+ */
+export const PATCH = async (request: Request, { params }: RouteParams) => {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return NextResponse.json({ error: '인증이 필요합니다.' }, { status: 403 });
+  }
+
+  try {
+    const { id } = params;
+    const body = (await request.json()) as UpdateProfile;
+    const { name, bio } = body;
+
+    const user = await prisma.user.findUnique({ where: { id } });
+    if (!user) {
+      return NextResponse.json(
+        { error: 'User를 찾을 수 없습니다.' },
+        { status: 404 },
+      );
+    }
+
+    // 유효성 검사
+    if (name && (name.trim().length < 2 || name.trim().length > 10)) {
+      return NextResponse.json(
+        { error: '닉네임은 1~10자여야 하며, 앞뒤 공백을 허용하지 않습니다.' },
+        { status: 400 },
+      );
+    }
+    if (bio && (bio.length < 1 || bio.length > 20)) {
+      return NextResponse.json(
+        { error: '자기소개는 1~20자여야 합니다.' },
+        { status: 400 },
+      );
+    }
+
+    const updatedProfile = await prisma.user.update({
+      where: { id },
+      data: {
+        ...(name !== undefined && { name: name.trim() }),
+        ...(bio !== undefined && { bio }),
+      },
+    });
+
+    return NextResponse.json(updatedProfile);
+  } catch (error) {
+    console.error('Profile 수정 에러:', error);
+    return NextResponse.json(
+      { error: 'Profile 수정에 실패했습니다.' },
+      { status: 500 },
+    );
+  }
+};
