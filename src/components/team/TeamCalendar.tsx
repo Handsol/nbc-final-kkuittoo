369494898e38eda4ee631d4f745bd46788539ae4
miing_gaeng <pem4134@naver.com>
2025-04-03 12:43:02 +0900

    Feat: Team 캘린더 컴포넌트 구현 #13

---
+++ src/components/team/TeamCalendar.tsx
@@ -0,0 +1,11 @@
+import { Calendar } from '../ui/calendar';
+
+const TeamCalendar = () => {
+  return (
+    <div className="flex flex-1 border border-black justify-center items-center">
+      <Calendar />
+    </div>
+  );
+};
+
+export default TeamCalendar;
