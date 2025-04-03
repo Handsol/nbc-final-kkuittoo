3747670d4194fbbc5ae3abf336afbe919f28fd20
miing_gaeng <pem4134@naver.com>
2025-04-03 23:48:32 +0900

    Style: Team 페이지 컴포넌트 스타일 #34

--- src/components/team/TeamInfo.tsx
+++ src/components/team/TeamInfo.tsx
@@ -4,6 +4,7 @@ import {
   fetchTeamData,
 } from '@/lib/services/team-actions.services';
 import { Progress } from '../ui/progress';
+import Image from 'next/image';
 
 type TeamQuestProps = {
   id: string;
@@ -23,33 +24,38 @@ const TeamInfo = async ({ id }: TeamQuestProps) => {
   const { teamName, teamBio, emblem } = teamData;
 
   return (
-    <div className="w-full flex border border-black">
-      <div className="flex flex-1 items-center gap-5">
+    <section className="w-full flex bg-neutral-400 rounded-3xl p-9 gap-5">
+      <div className="relative w-64 h-60">
+        <Image
+          src={'/teamQuest'}
+          alt="teamQuest"
+          fill
+          className="bg-neutral-500 rounded-3xl"
+        />
         <img
           src={emblem}
-          alt="team_emblem"
-          className="w-20 h-20 rounded-full bg-slate-300"
+          alt="emblem"
+          className="w-14 h-14 rounded-full bg-neutral-700 absolute bottom-3 right-3"
         />
-        <div>
-          <p>{teamName}</p>
-          <p>{teamBio}</p>
-        </div>
       </div>
-      <div className="flex flex-1 items-center gap-3">
-        <div className="flex-1">
-          <p>{teamCurrentQuest.questName}</p>
+      <article className="flex-1 flex flex-col gap-3 justify-center relative">
+        <section className="absolute right-0 top-8 flex flex-col justify-center items-center gap-3">
+          <button className="w-11 h-11 rounded-full bg-white">EDIT</button>
+          <button className="px-2 h-6 text-center bg-white rounded-full text-xs">
+            PRIVATE
+          </button>
+        </section>
+        <p className="font-bold text-2xl">{teamName}</p>
+        <p className="font-bold text-4xl">{teamCurrentQuest.questName}</p>
+        <section>
+          <Progress value={teamTotalPoints} className="w-full h-5" />
           <p>
             {teamTotalPoints}/{teamCurrentQuest.requiredPoints}
           </p>
-          <Progress value={teamTotalPoints} />
-        </div>
-        <img
-          src={teamCurrentQuest.questImage}
-          alt="teamQuest"
-          className="w-80 h-52 rounded-3xl bg-slate-300"
-        />
-      </div>
-    </div>
+        </section>
+        <p>{teamBio}</p>
+      </article>
+    </section>
   );
 };
 
