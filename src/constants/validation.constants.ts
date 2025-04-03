9ea3f8183b69a7f406fa9825ff50cb66076d7425
Handsol <imhandsol@gmail.com>
2025-04-03 22:32:53 +0900

    Feat: 유저 정보 유효성 검사 상수 추가 #30

--- src/constants/validation.constants.ts
+++ src/constants/validation.constants.ts
@@ -1,9 +1,22 @@
+// 팀 정보 유효성 검사
 export const TEAM_VALIDATAION = {
   TEAM_NAME: {
     MIN: 2,
     MAX: 10,
   },
   TEAM_BIO: {
+    MIN: 5,
+    MAX: 20,
+  },
+};
+
+// 유저 정보 유효성 검사
+export const USER_VALIDATION = {
+  NAME: {
+    MIN: 2,
+    MAX: 10,
+  },
+  BIO: {
     MIN: 5,
     MAX: 20,
   },
