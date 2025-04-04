78e7bb41b6b98851d4946b89ae8c75a466996c52
verdantgreeny <verdantgreeny@gmail.com>
2025-04-04 12:27:22 +0900

    Feat: 캘린더 추가 #32

--- src/components/mypage/MyPageCalendar.tsx
+++ src/components/mypage/MyPageCalendar.tsx
@@ -1,12 +1,14 @@
+import { Calendar } from '../ui/calendar';
+
 const MyPageCalendar = () => {
   return (
-    <section className="h-full bg-gray-300 p-6 rounded-3xl flex flex-col">
+    <section className="h-full bg-gray-200 p-6 rounded-3xl flex flex-col">
       <div className="mb-4">
-        <h1 className="text-xl font-bold">APRIL</h1>
+        <h1 className="text-xl font-bold">CALENDAL</h1>
       </div>
       <div className="flex-1 flex items-center justify-center min-h-0">
-        <div className="w-full h-full bg-gray-400 rounded flex items-center justify-center">
-          <p className="text-white">캘린더 영역</p>
+        <div className="w-full h-full rounded flex items-center justify-center bg-slate-50">
+          <Calendar />
         </div>
       </div>
     </section>
