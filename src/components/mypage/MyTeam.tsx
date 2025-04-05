478e7bf9dcbbfb742175a8b4e246830948ed7ff8
verdantgreeny <verdantgreeny@gmail.com>
2025-04-05 21:36:45 +0900

    Refactor: 마이페이지 팀정보 컴포넌트 분리 #32

---
+++ src/components/mypage/MyTeam.tsx
@@ -0,0 +1,49 @@
+import { Progress } from '@/components/ui/progress';
+import { TeamData } from '@/types/teams.type';
+import { TeamQuest } from '@prisma/client';
+import Image from 'next/image';
+
+type MyTeamProps = {
+  team: Pick<TeamData, 'id' | 'teamName' | 'teamBio' | 'emblem'>;
+  teamTotalPoints: number;
+  teamCurrentQuest: Pick<TeamQuest, 'questName' | 'requiredPoints'>;
+};
+
+const MyTeam = ({ team, teamTotalPoints, teamCurrentQuest }: MyTeamProps) => {
+  const contributionPercentage = Math.min(
+    (teamTotalPoints / teamCurrentQuest.requiredPoints) * 100,
+    100,
+  );
+
+  return (
+    <section className="h-full bg-gray-300 p-6 rounded-3xl flex flex-col">
+      <div className="mb-4">
+        <h1 className="text-xl font-bold">MY TEAM</h1>
+      </div>
+
+      <div className="w-full max-w-5xl flex flex-row gap-4">
+        <div className="w-2/5 flex items-center justify-center relative">
+          <Image
+            src="/images/test01.png"
+            alt="Team Quest Image"
+            fill
+            className="w-full h-full rounded-md object-cover"
+          />
+        </div>
+        <div className="w-3/5 flex flex-col gap-3 text-sm">
+          <h2 className="text-xl font-semibold">{team!.teamName}</h2>
+          <p className="text-gray-700 text-xl">{teamCurrentQuest!.questName}</p>
+          <section>
+            <Progress value={contributionPercentage} className="w-full h-5" />
+            <p>
+              {teamTotalPoints}/{teamCurrentQuest!.requiredPoints}
+            </p>
+          </section>
+          <p className="text-gray-600 truncate">{team!.teamBio}</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MyTeam;
---
+++ src/components/mypage/NoTeam.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+
+const NoTeam = () => {
+  return (
+    <section className="h-full bg-gray-300 p-6 rounded-3xl flex flex-col">
+      <div className="mb-4">
+        <h1 className="text-xl font-bold">MY TEAM</h1>
+      </div>
+      <div className="flex-1 flex items-center justify-center">
+        <div className="w-full max-w-5xl flex flex-row gap-6">
+          <div className="w-2/5 flex items-center justify-center relative">
+            <Image
+              src="/images/test01.png"
+              alt="Guild Image"
+              fill
+              className="w-full h-full rounded-md object-cover"
+            />
+          </div>
+          <div className="w-3/5 flex flex-col justify-between text-sm gap-4">
+            <p className="text-gray-600">
+              아직 팀에 가입하지 않았습니다. 새로운 팀을 찾거나 직접 팀을 만들어
+              보세요! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Nulla facilisi. Curabitur tincidunt, lacus a gravida vestibulum,
+              elit nunc gravida justo, vel fermentum erat neque a turpis.
+            </p>
+            <div className="flex gap-4 w-full mt-2">
+              <button className="flex-1 px-4 py-1.5 bg-gray-700 text-white rounded-full">
+                Find new Team
+              </button>
+              <button className="flex-1 px-4 py-1.5 bg-gray-700 text-white rounded-full">
+                Make new Team
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NoTeam;
