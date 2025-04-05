465fcec985596e32cc87a5f1995cb122c1157702
bonseung-dev <glynnregister@gmail.com>
2025-04-05 21:53:42 +0900

    Feat: 유저 랭킹 타입 정의 추가 #27

---
+++ src/types/rank-users.type.ts
@@ -0,0 +1,9 @@
+// 유저 랭킹에 필요한 타입
+export type UserData = {
+  id: string;
+  name: string;
+  bio: string;
+  email: string;
+  image: string;
+  userPoints: { points: number }[]; // 포인트 배열 (합산해서 랭킹 계산)
+};
