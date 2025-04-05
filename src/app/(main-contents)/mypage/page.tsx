10e2c1f6dcf5a22b2daabbd00a060d84492ffaec
verdantgreeny <verdantgreeny@gmail.com>
2025-04-05 17:40:21 +0900

    Fix: 프롭 드릴링 개선 #32

--- src/app/(main-contents)/mypage/page.tsx
+++ src/app/(main-contents)/mypage/page.tsx
@@ -2,13 +2,25 @@ import MyPageCalendar from '@/components/mypage/MyPageCalendar';
 import MyPageHabits from '@/components/mypage/MyPageHabits';
 import MyPageProfile from '@/components/mypage/MyPageProfile';
 import MyPageTeam from '@/components/mypage/MyPageTeam';
+import { authOptions } from '@/lib/utils/auth';
+import { getServerSession } from 'next-auth';
 
-const MyPage = () => {
+const MyPage = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    return (
+      <div className="flex items-center justify-center min-h-[calc(100vh-3rem)]">
+        <p className="text-lg">로그인이 필요합니다.</p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center px-[30px] py-6 min-h-[calc(100vh-3rem)] gap-10">
       <div className="w-full max-w-[680px] flex gap-6">
         <div className="w-[370px] h-[640px]">
-          <MyPageHabits />
+          <MyPageHabits userId={session.user.id} />
         </div>
 
         <div className="w-[280px] h-[640px] flex flex-col gap-4">
--- src/components/mypage/MyPageHabits.tsx
+++ src/components/mypage/MyPageHabits.tsx
@@ -1,37 +1,19 @@
-import { getServerSession } from 'next-auth';
-import AddHabitButton from './AddHabitButton';
-import HabitList from './HabitList';
-import { fetchGetAllHabits } from '@/lib/services/habit-actions.services';
-import { authOptions } from '@/lib/utils/auth';
+'use client';
 
-const MyPageHabits = async () => {
-  const session = await getServerSession(authOptions);
-  if (!session?.user) {
-    return <div className="p-4">로그인이 필요합니다.</div>;
-  }
-  const habits = await fetchGetAllHabits(session.user.id);
-  const currentHabitCount = habits.length;
+import { useHabitsQuery } from '@/lib/queries/habits.queries';
+import HabitHeader from './habits/HabitHeader';
+import HabitContent from './habits/HabitContent';
 
+type MyPageHabitsProps = {
+  userId: string;
+};
+
+const MyPageHabits = ({ userId }: MyPageHabitsProps) => {
+  const { data: habits = [] } = useHabitsQuery(userId);
   return (
     <section className="h-full bg-gray-100 p-6 rounded-3xl flex flex-col">
-      <div className="flex items-center gap-2 mb-4">
-        <h1 className="text-xl font-bold text-gray-800">HABITS</h1>
-        <span className="text-sm text-gray-500">({currentHabitCount}/10)</span>
-      </div>
-
-      <div className="flex-1 overflow-y-auto">
-        {habits.length > 0 ? (
-          <HabitList habits={habits} />
-        ) : (
-          <div className="h-full flex items-center justify-center text-gray-500">
-            등록된 habit이 없습니다.
-          </div>
-        )}
-      </div>
-
-      <div className="mt-4 w-full">
-        <AddHabitButton userId={session.user.id} />
-      </div>
+      <HabitHeader habitsLength={habits.length} />
+      <HabitContent habits={habits} userId={userId} />
     </section>
   );
 };
