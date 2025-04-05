fb25cb4f181a5af4d4ee25d3167f3c23ef8988f1
bonseung-dev <glynnregister@gmail.com>
2025-04-05 21:58:49 +0900

    Fix: 상수화한 쿼리 키 적용

--- src/lib/queries/useTeamQuery.ts
+++ src/lib/queries/useTeamQuery.ts
@@ -1,12 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchGetTeams } from '../services/team.services';
 import { TeamData } from '@/types/teams';
+import { QUERY_KEYS } from '@/constants/query-keys.constants';
 
 // 팀 데이터 가져오기 훅
 export const useTeamQuery = () => {
   return useQuery<TeamData[]>({
     // TeamData 배열 반환
-    queryKey: ['teams'],
+    queryKey: QUERY_KEYS.TEAMS,
     queryFn: fetchGetTeams, // fetchTeams 호출 데이터 가져오기
   });
   // {data, isPending, isError} 같은 정보 반환
