3694a725d25498cd7514a008e9bf5debd54b5f83
verdantgreeny <verdantgreeny@gmail.com>
2025-04-05 17:37:51 +0900

    Fix: 리턴값 수전 #32

--- src/lib/services/habit-actions.services.ts
+++ src/lib/services/habit-actions.services.ts
@@ -1,14 +1,16 @@
 'use server';
 
-import { Habit } from '@prisma/client';
+import { Habit, UserPoint } from '@prisma/client';
 import { prisma } from '../prisma';
 
 /**
  * 사용자의 모든 Habit 목록을 조회하는 함수
  * @param {string} userId - 조회할 사용자의 ID
- * @returns {Promise<Habit[]>} - 사용자의 Habit 목록
+ * @returns {Promise<(Habit & { userPoints: UserPoint[] })[]>} - 사용자의 Habit 목록 (userPoints 포함)
  */
-export const fetchGetAllHabits = async (userId: string): Promise<Habit[]> => {
+export const fetchGetAllHabits = async (
+  userId: string,
+): Promise<(Habit & { userPoints: UserPoint[] })[]> => {
   return await prisma.habit.findMany({
     where: { userId },
     include: { userPoints: true },
