0c233d343dfa2baa4156f0d8cd4e93c37c63cd39
bonseung-dev <glynnregister@gmail.com>
2025-04-06 17:58:56 +0900

    Feat: 멤버 총 포인트, 멤버 인원 수 타입 정의 #27

--- src/types/rank-users.type.ts
+++ src/types/rank-users.type.ts
@@ -1,3 +1,5 @@
+import { TeamData } from './teams.type';
+
 // 유저 랭킹에 필요한 타입
 export type UserData = {
   id: string;
@@ -6,4 +8,9 @@ export type UserData = {
   email: string;
   image: string;
   userPoints: { points: number }[]; // 포인트 배열 (합산해서 랭킹 계산)
+};
+
+export type TeamWithPoints = TeamData & {
+  totalPoints: number;
+  memberCount: number;
 };
