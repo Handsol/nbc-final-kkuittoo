dde544c4bdf48aa9731d81e57d578e5cd5629f8a
Handsol <imhandsol@gmail.com>
2025-04-06 15:19:16 +0900

    Feat: 로그인 성공 시 토스트 메세지 표시 기능 추가 #30

--- src/components/login/GoogleLogin.tsx
+++ src/components/login/GoogleLogin.tsx
@@ -5,15 +5,20 @@ import Image from 'next/image';
 import { useEffect } from 'react';
 import LogoutButton from './LogoutButton';
 import LoginButton from './LoginButton';
+import { useToast } from '@/hooks/use-toast';
 
 const GoogleLogin = () => {
   const { data: session, status } = useSession();
   const isLoading = status === 'loading';
+  const { toast } = useToast();
 
   // local storage에 로그인 상태 저장
   useEffect(() => {
     if (status === 'authenticated') {
       localStorage.setItem('islogined', 'true');
+
+      // 로그인 성공 시 토스트 메세지 표시
+      toast({ description: '로그인 성공' });
     } else if (status === 'unauthenticated') {
       localStorage.removeItem('islogined');
     }
