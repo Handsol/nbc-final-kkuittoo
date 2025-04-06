238611dd2249b39fee6c9e75ee434c54a20e08ad
verdantgreeny <verdantgreeny@gmail.com>
2025-04-06 21:03:00 +0900

    Cleanup: 주석 추가 #44

--- src/lib/services/habit-client.services.ts
+++ src/lib/services/habit-client.services.ts
@@ -1,6 +1,11 @@
 import { CreateHabit, UpdateHabit } from '@/types/mypage.type';
 import { Habit } from '@prisma/client';
 
+/**
+ * 새로운 Habit을 생성하는 API 요청 함수
+ * @param {CreateHabit} data - 생성할 Habit의 정보
+ * @returns {Promise<Habit>} 생성된 Habit
+ */
 export const fetchCreateHabit = async (data: CreateHabit): Promise<Habit> => {
   const res = await fetch('/api/habits', {
     method: 'POST',
@@ -11,6 +16,12 @@ export const fetchCreateHabit = async (data: CreateHabit): Promise<Habit> => {
   return res.json();
 };
 
+/**
+ * 선택한 Habit을 수정하는 API 요청 함수
+ * @param {string} id - 수정할 Habit의 ID
+ * @param {UpdateHabit} data - 수정할 데이터
+ * @returns {Promise<Habit>} 수정된 Habit
+ */
 export const fetchUpdateHabit = async (
   id: string,
   data: UpdateHabit,
@@ -24,6 +35,11 @@ export const fetchUpdateHabit = async (
   return res.json();
 };
 
+/**
+ * 선택한 Habit을 삭제하는 API 요청 함수
+ * @param {string} id - 삭제할 Habit의 ID
+ * @returns {Promise<void>}
+ */
 export const fetchDeleteHabit = async (id: string): Promise<void> => {
   const res = await fetch(`/api/habits/${id}`, { method: 'DELETE' });
   if (!res.ok) throw new Error('habit 삭제에 실패하였습니다.');
