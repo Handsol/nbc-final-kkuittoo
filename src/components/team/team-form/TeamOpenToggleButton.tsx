6ed72e51907dcacf22dc0bedc9978734e0400117
miing_gaeng <pem4134@naver.com>
2025-04-06 22:23:14 +0900

    Feat: 팀생성자 여부에 따른 isOpened 버튼 컴포넌트 분리 후 구현 #36

---
+++ src/components/team/team-form/TeamOpenToggleButton.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Switch } from '@/components/ui/switch';
+import { useTeamOpenMutation } from '@/lib/mutations/useTeamOpenMutation';
+import { useSingleTeamQuery } from '@/lib/queries/useSingleTeamQuery';
+
+type TeamOpenToggleButtonProps = {
+  teamId: string;
+};
+
+const TeamOpenToggleButton = ({ teamId }: TeamOpenToggleButtonProps) => {
+  // tanstack query - useQuery
+  const { data: teamData, isLoading } = useSingleTeamQuery(teamId);
+  // tanstack query - useMutation
+  const { mutate } = useTeamOpenMutation(teamId);
+
+  if (isLoading || !teamData) return <p>로딩 중...</p>;
+
+  return (
+    <div className="flex items-center gap-2">
+      <Switch
+        checked={teamData.isOpened}
+        onCheckedChange={(checked) => mutate(checked)}
+      />
+      <label>{teamData.isOpened ? 'OPEN' : 'PRIVATE'}</label>
+    </div>
+  );
+};
+
+export default TeamOpenToggleButton;
