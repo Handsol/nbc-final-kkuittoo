966d59644315e37fac8cb41c6d70cd274d3679ac
Handsol <imhandsol@gmail.com>
2025-04-07 17:13:10 +0900

    Feat: UserTitle 컴포넌트 추가 #55

---
+++ src/components/common/UserTitle.tsx
@@ -0,0 +1,23 @@
+import { USER_TITLE_MODE } from '@/constants/mode.constants';
+
+type UserTitleProps = {
+  mode: number;
+  children: string;
+};
+
+const Title = ({ mode, children }: UserTitleProps) => {
+  switch (mode) {
+    case USER_TITLE_MODE.CARD_LEVEL:
+      return <h5>{children}</h5>;
+    case USER_TITLE_MODE.CARD_NAME:
+      return <h5>{children}</h5>;
+    case USER_TITLE_MODE.CARD_RANK:
+      return <h5>{children}</h5>;
+    case USER_TITLE_MODE.CARD_ID:
+      return <h6>{children}</h6>;
+    default:
+      return <span>{children}</span>; // fallback
+  }
+};
+
+export default Title;
