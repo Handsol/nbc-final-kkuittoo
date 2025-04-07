87ee958c36779294f2f374bebd9a6e93e6d5268c
bonseung-dev <glynnregister@gmail.com>
2025-04-07 21:15:06 +0900

    Feat: 공통 모달 컴포넌트 구현 #54

---
+++ src/components/common/commonModal.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { ReactNode } from 'react';
+
+type CommonModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+};
+
+export const CommonModal = ({
+  isOpen = false, // 기본값 설정
+  onClose,
+  children,
+}: CommonModalProps) => {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="fixed inset-0 z-50 bg-black bg-opacity-60 flex justify-center items-center"
+      onClick={onClose}
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="bg-white rounded-2xl p-8 max-w-[30%] max-h-[70%] w-full h-auto overflow-auto shadow-xl"
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
