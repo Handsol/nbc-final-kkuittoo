c11e8df03eb1d1edc9ac726dc178a8c5cb940808
bonseung-dev <glynnregister@gmail.com>
2025-04-08 09:15:00 +0900

    Cleanup: JSDocs 추가 #54

--- src/components/common/commonModal.tsx
+++ src/components/common/commonModal.tsx
@@ -1,7 +1,26 @@
 'use client';
 
 import { ReactNode } from 'react';
-
+/**
+ * 공통 모달 컴포넌트입니다.
+ *
+ * 화면 중앙에 모달을 띄우며, 외부 영역(배경)을 클릭하면 닫히도록 설정했습니다.
+ * 모달 내부를 클릭하면 닫히지 않도록 클릭 이벤트를 주었습니다.
+ *
+ * @component
+ *
+ * @param {boolean} isOpen - 기본값은 `false`이며, `true`일 때만 모달이 보입니다.
+ * @param {() => void} onClose - 모달을 닫을 때 실행할 함수입니다. (예: 상태 초기화)
+ * @param {ReactNode} children - 모달 내부에 렌더링할 콘텐츠입니다. (예: 상세 정보, 폼 등)
+ *
+ * @example
+ *
+ * ```tsx
+ * <CommonModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+ *   <p>모달 내용</p>
+ * </CommonModal>
+ * ```
+ */
 type CommonModalProps = {
   isOpen: boolean;
   onClose: () => void;
