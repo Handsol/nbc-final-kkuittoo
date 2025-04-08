e0807dff62e36fa66d614ea12e9a180e689957c6
verdantgreeny <verdantgreeny@gmail.com>
2025-04-09 00:52:25 +0900

    Feat: 마이페이지 메타데이터 추가 #66

--- src/app/(main-contents)/mypage/page.tsx
+++ src/app/(main-contents)/mypage/page.tsx
@@ -4,7 +4,30 @@ import MyPageHabits from '@/components/mypage/MyPageHabits';
 import MyPageProfile from '@/components/mypage/MyPageProfile';
 import MyPageTeam from '@/components/mypage/MyPageTeam';
 import { authOptions } from '@/lib/utils/auth';
+import { Metadata } from 'next';
 import { getServerSession } from 'next-auth';
+
+export const metadata: Metadata = {
+  title: '마이페이지 | KKUITTOO',
+  description:
+    '사용자의 습관, 프로필 및 팀 정보를 확인할 수 있는 마이페이지입니다.',
+  keywords: ['마이페이지', '습관', '팀'],
+  openGraph: {
+    title: '마이페이지 | KKUITTOO',
+    description:
+      '사용자의 습관, 프로필 및 팀 정보를 확인할 수 있는 마이페이지입니다.',
+    type: 'website',
+    url: 'http://localhost:3000/mypage',
+    images: [
+      {
+        url: '/images/test01.png',
+        width: 1200,
+        height: 630,
+        alt: '마이페이지 미리보기',
+      },
+    ],
+  },
+};
 
 const MyPage = async () => {
   const session = await getServerSession(authOptions);
