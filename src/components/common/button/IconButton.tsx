5ce59df13ab6cc7a11dffc2d17012e2e0d06e157
verdantgreeny <verdantgreeny@gmail.com>
2025-04-08 12:32:08 +0900

    Add: IconButton 컴포넌트 생성 #58

--- src/components/common/button/IconButton.tsx
+++ src/components/common/button/IconButton.tsx
@@ -1,36 +1,61 @@
 import { ButtonHTMLAttributes } from 'react';
 import { Pencil, X, Plus } from 'lucide-react';
+import { ICONBUTTON_MODE } from '@/constants/mode.constants';
 
 type IconButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  variant: 'edit' | 'delete' | 'add';
+  mode: string;
+  className?: string;
+  disabled?: boolean;
 };
 
-const IconButton = ({ variant, className = '', ...props }: IconButtonProps) => {
-  let icon;
-  let baseClasses =
+/**
+ * IconButton 컴포넌트
+ * @typedef {Object} IconButtonProps
+ * @property {string} mode - 버튼 모드 (예: 'edit', 'delete', 'add')
+ * @property {string} [className] - 추가적인 CSS 클래스 - 선택적
+ * @property {boolean} [disabled] - 버튼 비활성화 여부 - 선택적 (기본값: false)
+ * @extends {ButtonHTMLAttributes<HTMLButtonElement>}
+ */
+const IconButton = ({
+  mode,
+  className = '',
+  disabled = false,
+  ...props
+}: IconButtonProps) => {
+  const baseClasses =
     'w-9 h-9 flex items-center justify-center rounded-full transition-colors shrink-0';
-  let variantClasses = '';
 
-  if (variant === 'edit') {
-    icon = <Pencil className="w-4 h-4" />;
-    variantClasses = 'bg-gray-100 hover:bg-gray-200 text-gray-800';
+  let icon;
+  let variantClasses;
+
+  switch (mode) {
+    case ICONBUTTON_MODE.EDIT:
+      icon = <Pencil className="w-4 h-4" />;
+      variantClasses = 'bg-gray-100 hover:bg-gray-200 text-gray-800';
+      break;
+    case ICONBUTTON_MODE.DELETE:
+      icon = <X className="w-4 h-4" />;
+      variantClasses = 'bg-gray-100 hover:bg-gray-200 text-gray-800';
+      break;
+    case ICONBUTTON_MODE.ADD:
+      icon = <Plus className="w-5 h-5" />;
+      variantClasses = 'bg-gray-200 text-gray-800 hover:bg-gray-300';
+      break;
+    default:
+      icon = <Plus className="w-5 h-5" />;
+      variantClasses = 'bg-gray-200 text-gray-800 hover:bg-gray-300';
+      break;
   }
 
-  if (variant === 'delete') {
-    icon = <X className="w-4 h-4" />;
-    variantClasses = 'bg-gray-100 hover:bg-gray-200 text-gray-800';
-  }
-
-  if (variant === 'add') {
-    icon = <Plus className="w-5 h-5" />;
-    variantClasses =
-      'bg-gray-200 text-gray-800 hover:bg-gray-300 text-lg font-extrabold';
-  }
+  const disabledClasses = disabled
+    ? 'bg-gray-300 text-gray-400 cursor-not-allowed'
+    : '';
 
   return (
     <button
-      className={`${baseClasses} ${variantClasses} ${className}`}
-      aria-label={variant}
+      className={`${baseClasses} ${variantClasses} ${disabledClasses} ${className}`}
+      aria-label={mode}
+      disabled={disabled}
       {...props}
     >
       {icon}
