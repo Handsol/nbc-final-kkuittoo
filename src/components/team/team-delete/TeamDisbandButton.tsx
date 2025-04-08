06b41bbee1f14663a2c4236295b40fccf7ec82ec
miing_gaeng <pem4134@naver.com>
2025-04-08 15:37:00 +0900

    Refactor: filter에서 some으로 로직 수정 #61

--- src/components/team/team-delete/TeamDisbandButton.tsx
+++ src/components/team/team-delete/TeamDisbandButton.tsx
@@ -23,9 +23,9 @@ const TeamDisbandButton = ({
   // route
   const router = useRouter();
   // 다른 팀원 있는지 여부 판단
-  const membersExceptTeamOwner = memberList.filter(
+  const membersExceptTeamOwner = memberList.some(
     (member) => member.userId !== ownerId,
-  ).length;
+  );
 
   // confirmDialog에 들어갈 컨텐츠
   const disbandContents = {
@@ -38,7 +38,7 @@ const TeamDisbandButton = ({
 
   const handleDisbandBtnClick = async () => {
     // 팀장 외 다른 팀원이 있는 경우 예외처리
-    if (!!membersExceptTeamOwner) {
+    if (membersExceptTeamOwner) {
       toast({
         title: '남은 팀원이 있어요!',
         description: '팀장을 제외한 다른 멤버가 없을 때, 해체가 가능해요!',
