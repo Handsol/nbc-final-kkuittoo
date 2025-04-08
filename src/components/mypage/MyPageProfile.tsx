04d4742d537a23dc7822efc30cae85ee8de870ae
Handsol <imhandsol@gmail.com>
2025-04-08 17:05:32 +0900

    Feat: 사용자 프로필 정보를 비동기로 가져오는 기능 추가, 고유 아이디 뒤 8글자만 가져오도록 slice #48

--- src/components/mypage/MyPageProfile.tsx
+++ src/components/mypage/MyPageProfile.tsx
@@ -1,15 +1,28 @@
+import { fetchGetUserProfile } from '@/lib/services/user-actions.services';
+import Text from '../common/Text';
+import { USER_TITLE_MODE } from '@/constants/mode.constants';
+import UserTitle from '../common/UserTitle';
+
 type MyPageHabitsProps = {
   userId: string;
 };
 
-const MyPageProfile = ({ userId }: MyPageHabitsProps) => {
+const MyPageProfile = async ({ userId }: MyPageHabitsProps) => {
+  const profileData = await fetchGetUserProfile(userId);
+
+  if (!profileData) return <Text>존재하지 않는 유저입니다.</Text>;
+
   return (
     <div className="flex-[2] p-4 rounded-xl border shadow-sm bg-white">
       <div className="flex items-center justify-evenly">
-        <span className="text-2xl font-bold">Lv.1</span>
+        <UserTitle mode={USER_TITLE_MODE.CARD_LEVEL}>Lv.1</UserTitle>
         <div className="text-center">
-          <p className="text-lg font-bold">닉네임</p>
-          <p className="text-sm text-gray-500">@고유아이디</p>
+          <UserTitle mode={USER_TITLE_MODE.CARD_NAME}>
+            {profileData.name}
+          </UserTitle>
+          <UserTitle mode={USER_TITLE_MODE.CARD_ID}>
+            @{profileData.id.slice(-8)}
+          </UserTitle>
         </div>
         <button className="px-4 py-2 text-sm font-medium text-white bg-gray-500 rounded-full">
           수정
@@ -18,7 +31,7 @@ const MyPageProfile = ({ userId }: MyPageHabitsProps) => {
       <div className="mt-4 h-2 w-full bg-gray-200 rounded-full">
         <div className="h-2 w-1/2 bg-gray-500 rounded-full"></div>
       </div>
-      <p className="mt-4 text-gray-700 text-center">소개글</p>
+      <Text>{profileData.bio}</Text>
     </div>
   );
 };
