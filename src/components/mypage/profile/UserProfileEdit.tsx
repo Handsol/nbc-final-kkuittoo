8f2ff56f9ee7799660d8eb486ed573e9ec141359
Handsol <imhandsol@gmail.com>
2025-04-08 22:52:28 +0900

    Feat: 사용자 프로필 수정 시 닉네임 및 자기소개 유효성 검사 추가 #48

--- src/components/mypage/profile/UserProfileEdit.tsx
+++ src/components/mypage/profile/UserProfileEdit.tsx
@@ -9,6 +9,8 @@ import { useUserProfileMutation } from '@/lib/mutations/useUserProfileMutation';
 import { UserFormData } from '@/lib/services/user-client.services';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { USER_VALIDATION } from '@/constants/validation.constants';
+import { toast } from '@/hooks/use-toast';
 
 type Props = {
   name: string;
@@ -26,6 +28,34 @@ const UserProfileEdit = ({ name, bio, userId }: Props) => {
   const { mutate: updateUser, isPending } = useUserProfileMutation(userId);
 
   const onSubmit = (data: UserFormData) => {
+    const { name, bio } = data;
+
+    // 닉네임 검사
+    if (
+      name.trim().length < USER_VALIDATION.NAME.MIN ||
+      name.trim().length > USER_VALIDATION.NAME.MAX
+    ) {
+      toast({
+        title: '닉네임 오류',
+        description: `닉네임은 ${USER_VALIDATION.NAME.MIN}자 이상, ${USER_VALIDATION.NAME.MAX}자 이하여야 합니다.`,
+        variant: 'destructive',
+      });
+      return;
+    }
+
+    // 자기소개 검사
+    if (
+      bio.trim().length < USER_VALIDATION.BIO.MIN ||
+      bio.trim().length > USER_VALIDATION.BIO.MAX
+    ) {
+      toast({
+        title: '소개 오류',
+        description: `소개는 ${USER_VALIDATION.BIO.MIN}자 이상, ${USER_VALIDATION.BIO.MAX}자 이하여야 합니다.`,
+        variant: 'destructive',
+      });
+      return;
+    }
+
     updateUser(data, {
       onSuccess: () => {
         setIsEditMode(false);
