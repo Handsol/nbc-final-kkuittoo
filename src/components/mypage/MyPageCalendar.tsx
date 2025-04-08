e9302efc09977edbc404d989b3a16a02817e982a
verdantgreeny <verdantgreeny@gmail.com>
2025-04-08 16:29:19 +0900

    Refactor: Title 컴포넌트 적용 #66

--- src/components/mypage/MyPageCalendar.tsx
+++ src/components/mypage/MyPageCalendar.tsx
@@ -1,10 +1,12 @@
+import { TITLE_MODE } from '@/constants/mode.constants';
+import Title from '../common/Title';
 import { Calendar } from '../ui/calendar';
 
 const MyPageCalendar = () => {
   return (
     <section className="h-full bg-gray-200 p-6 rounded-3xl flex flex-col">
       <div className="mb-4">
-        <h1 className="text-xl font-bold">CALENDAL</h1>
+        <Title mode={TITLE_MODE.SECTION_TITLE}>CALENDAL</Title>
       </div>
       <div className="flex-1 flex items-center justify-center min-h-0">
         <div className="w-full h-full rounded flex items-center justify-center">
--- src/components/mypage/MyPageHabits.tsx
+++ src/components/mypage/MyPageHabits.tsx
@@ -2,6 +2,8 @@
 
 import { useHabitsQuery } from '@/lib/queries/useHabitsQuery';
 import HabitContent from './habits/HabitContent';
+import { TITLE_MODE } from '@/constants/mode.constants';
+import Title from '../common/Title';
 
 type MyPageHabitsProps = {
   userId: string;
@@ -13,7 +15,7 @@ const MyPageHabits = ({ userId }: MyPageHabitsProps) => {
   return (
     <section className="h-full bg-gray-100 p-6 rounded-3xl flex flex-col">
       <div className="flex items-center gap-2 mb-4">
-        <h1 className="text-xl font-bold text-gray-800">HABITS</h1>
+        <Title mode={TITLE_MODE.SECTION_TITLE}>HABITS</Title>
         <span className="text-sm text-gray-500">({habits.length}/10)</span>
       </div>
       <HabitContent habits={habits} userId={userId} />
