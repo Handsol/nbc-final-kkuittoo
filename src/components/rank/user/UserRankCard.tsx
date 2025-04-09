b363d8d50f866e2d768eaa0966444536e468c15c
bonseung-dev <glynnregister@gmail.com>
2025-04-09 12:59:24 +0900

    Fix: import 오류 수정 #73

--- src/components/rank/user/UserRankCard.tsx
+++ src/components/rank/user/UserRankCard.tsx
@@ -2,7 +2,7 @@
 
 import { UserCardProps } from '@/types/rank.type';
 import { TopRankCard } from './UserTopRankCard';
-import { NormalRankCard } from './UserNomalRankCard';
+import { NormalRankCard } from './UserNormalRankCard';
 
 // 유저 정보를 카드로 보여주는 컴포넌트
 // isTopRank로 상단 3위와 나머지 스타일 구별
