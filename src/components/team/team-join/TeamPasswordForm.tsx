1baa177721b3584c281135086aa215fa67e3b2a2
miing_gaeng <pem4134@naver.com>
2025-04-10 09:08:37 +0900

    Fix: FormData 타입명 수정 #77

--- src/components/team/team-join/TeamPasswordForm.tsx
+++ src/components/team/team-join/TeamPasswordForm.tsx
@@ -14,7 +14,7 @@ import { checkTeamPassword } from '@/lib/utils/team-validation.utils';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 
-type FormData = {
+type TeamPasswordFormData = {
   teamPassword: string;
 };
 
@@ -32,10 +32,10 @@ const TeamPasswordForm = ({ teamId }: TeamPasswordFormProps) => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<TeamPasswordFormData>();
 
   // 비공개 팀 가입 로직
-  const onSubmit = async (data: FormData) => {
+  const onSubmit = async (data: TeamPasswordFormData) => {
     // 사용자 입력값과 비밀번호 일치 여부 판단
     const isCorrectPassword = checkTeamPassword(teamId, data.teamPassword);
     if (!isCorrectPassword) {
