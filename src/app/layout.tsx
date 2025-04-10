0e414d1cee6b937162c46b690d075065d394f98e
miing_gaeng <pem4134@naver.com>
2025-04-10 14:34:23 +0900

    Cleanup: 필요없는 코드 삭제 #77

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import Provider from '@/lib/providers/Provider';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title: 'KKUITTOO',
@@ -27,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Provider>
           {children}
           <Toaster />
