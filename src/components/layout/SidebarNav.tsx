4bd8f87eb914e83847038f65dccdaf7ebb066781
Handsol <imhandsol@gmail.com>
2025-04-11 01:10:42 +0900

    Refactor: SidebarNav 컴포넌트 분리 #86

---
+++ src/components/layout/SidebarNav.tsx
@@ -0,0 +1,37 @@
+import { LINKBUTTON_MODE } from '@/constants/mode.constants';
+import { PATH } from '@/constants/path.constants';
+import LinkButton from '../common/button/LinkButton';
+import { getServerSession } from 'next-auth';
+import { fetchGetMyTeamData } from '@/lib/services/team-actions.services';
+import { authOptions } from '@/lib/utils/auth';
+
+const NAV_ITEMS = [
+  { name: 'DASHBOARD', href: PATH.MYPAGE },
+  { name: 'MY TEAM', href: PATH.TEAM },
+  { name: 'RANK', href: PATH.RANK.USERS },
+];
+
+const SidebarNav = async () => {
+  const session = await getServerSession(authOptions);
+
+  let teamId = '';
+
+  if (session) {
+    const myTeam = await fetchGetMyTeamData(session.user.id);
+    if (myTeam) {
+      teamId = myTeam.teamId;
+    }
+  }
+
+  return (
+    <div className="w-full flex flex-col gap-2">
+      {NAV_ITEMS.map((item) => (
+        <LinkButton key={item.name} mode={LINKBUTTON_MODE.NAV} href={item.href}>
+          {item.name}
+        </LinkButton>
+      ))}
+    </div>
+  );
+};
+
+export default SidebarNav;
