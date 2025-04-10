db7d2b86b93e276685124a2d147443917d7abdbd
Handsol <imhandsol@gmail.com>
2025-04-11 01:14:05 +0900

    Refactor: Sidebar 컴포넌트에서 불필요한 코드 제거 및 구조 정리 #86

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,55 +1,14 @@
-import Image from 'next/image';
-import LinkButton from '../common/button/LinkButton';
 import LogoutButton from '../login/LogoutButton';
-import { PATH } from '@/constants/path.constants';
-import { LINKBUTTON_MODE } from '@/constants/mode.constants';
-import { getServerSession } from 'next-auth';
-import { fetchGetMyTeamData } from '@/lib/services/team-actions.services';
-import { authOptions } from '@/lib/utils/auth';
+import SidebarProfile from './SidebarProfile';
+import SidebarLogo from './SidebarLogo';
+import SidebarNav from './SidebarNav';
 
-const Sidebar = async () => {
-  const session = await getServerSession(authOptions);
-
-  let teamId = '';
-
-  if (session) {
-    const myTeam = await fetchGetMyTeamData(session.user.id);
-    if (myTeam) {
-      teamId = myTeam.teamId;
-    }
-  }
-
+const Sidebar = () => {
   return (
     <aside className="fixed top-0 h-screen w-[200px] bg-white p-4 flex flex-col gap-4 shrink-0 items-center">
-      <Image
-        src="/assets/images/logo_test.png"
-        alt="logo"
-        width={153}
-        height={20}
-      />
-
-      <div className="w-full flex flex-col gap-2">
-        <LinkButton mode={LINKBUTTON_MODE.NAV} href={PATH.MYPAGE}>
-          DASHBOARD
-        </LinkButton>
-        <LinkButton
-          mode={LINKBUTTON_MODE.NAV}
-          href={teamId ? `${PATH.TEAM}/${teamId}` : PATH.TEAM}
-        >
-          MY TEAM
-        </LinkButton>
-        <LinkButton mode={LINKBUTTON_MODE.NAV} href={PATH.RANK.USERS}>
-          RANK
-        </LinkButton>
-      </div>
-
-      <Image
-        src="/assets/images/user_lv1.png"
-        alt="user"
-        width={150}
-        height={150}
-      />
-
+      <SidebarLogo />
+      <SidebarNav />
+      <SidebarProfile />
       <LogoutButton />
     </aside>
   );
