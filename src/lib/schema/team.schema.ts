df60ea1773d888b397ed590cb55b55fafd4dc775
miing_gaeng <pem4134@naver.com>
2025-04-10 11:20:55 +0900

    Refactor: zod 적용 #77

--- src/lib/schema/team.schema.ts
+++ src/lib/schema/team.schema.ts
@@ -2,7 +2,7 @@ import { TEAMS_MESSAGES } from '@/constants/error-messages.constants';
 import { TEAM_VALIDATION } from '@/constants/validation.constants';
 import { z } from 'zod';
 
-const baseTeamSchema = z.object({
+export const teamTeamSchema = z.object({
   teamName: z
     .string()
     .min(TEAM_VALIDATION.TEAM_NAME.MIN, TEAMS_MESSAGES.TEAM_NAME_LENGTH)
@@ -13,4 +13,9 @@ const baseTeamSchema = z.object({
     .min(TEAM_VALIDATION.TEAM_BIO.MIN, TEAMS_MESSAGES.TEAM_BIO_LENGTH)
     .max(TEAM_VALIDATION.TEAM_BIO.MAX, TEAMS_MESSAGES.TEAM_BIO_LENGTH)
     .transform((value) => value.trim()),
+  maxTeamSize: z.string(),
+  emblem: z.string(),
+  isOpened: z.boolean(),
 });
+
+export type TeamFormSchema = z.infer<typeof teamTeamSchema>;
