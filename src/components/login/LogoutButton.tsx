3c5e6f1cbe8f1e06ad4f0eca3971b607510d0159
Handsol <imhandsol@gmail.com>
2025-04-10 14:11:16 +0900

    Refactor: ACTIONBUTTON_MODE 상수를 사용하여 로그아웃 버튼 모드 변경 #74

--- src/components/login/LogoutButton.tsx
+++ src/components/login/LogoutButton.tsx
@@ -3,11 +3,12 @@
 import { PATH } from '@/constants/path.constants';
 import { signOut } from 'next-auth/react';
 import ActionButton from '../common/button/ActionButton';
+import { ACTIONBUTTON_MODE } from '@/constants/mode.constants';
 
 const LogoutButton = () => {
   return (
     <ActionButton
-      mode="primary"
+      mode={ACTIONBUTTON_MODE.PRIMARY}
       onClick={() => signOut({ callbackUrl: PATH.HOME })}
     >
       로그아웃
