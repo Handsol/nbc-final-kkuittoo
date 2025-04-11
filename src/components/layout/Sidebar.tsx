a53b9bf7c19d8e48363314df20cb2d724327cef6
Handsol <imhandsol@gmail.com>
2025-04-12 00:27:56 +0900

    Feat: Sidebar 컴포넌트에 HowToUseButton 추가 #86

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -2,12 +2,14 @@ import LogoutButton from '../login/LogoutButton';
 import SidebarProfile from './SidebarProfile';
 import SidebarLogo from './SidebarLogo';
 import SidebarNav from './SidebarNav';
+import HowToUseButton from './HowToUseButton';
 
 const Sidebar = () => {
   return (
     <aside className="sticky top-0 h-screen w-[240px] bg-white p-4 flex flex-col gap-4 shrink-0 items-center">
       <SidebarLogo />
       <SidebarNav />
+      <HowToUseButton />
       <SidebarProfile />
       <LogoutButton />
     </aside>
