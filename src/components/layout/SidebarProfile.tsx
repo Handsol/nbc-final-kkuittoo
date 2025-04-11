04420df50adc73b1811d49ea8181abe1f26b68cd
Handsol <imhandsol@gmail.com>
2025-04-11 23:29:20 +0900

    Feat: 사이드바 프로필 컴포넌트에 사용자 세션 및 프로필 데이터 추가 #86

--- src/components/layout/SidebarProfile.tsx
+++ src/components/layout/SidebarProfile.tsx
@@ -1,13 +1,37 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+import { useUserQuery } from '@/lib/queries/useUserQuery';
 import Image from 'next/image';
+import UserProfileEdit from '../mypage/profile/UserProfileEdit';
+import UserTitle from '../common/UserTitle';
+import Text from '../common/Text';
+import { USER_TITLE_MODE } from '@/constants/mode.constants';
+import { ID_SLICE } from '@/constants/magic-numbers.constants';
 
 const SidebarAvatar = () => {
+  const { data: session } = useSession();
+  const userId = session?.user.id;
+
+  const { data: profileData, isPending } = useUserQuery(userId || '');
+
+  if (isPending) return <Text>로딩 중...</Text>;
+
   return (
-    <Image
-      src="/assets/images/user_lv1.png"
-      alt="user"
-      width={150}
-      height={150}
-    />
+    <div className="mt-[100px] flex flex-col items-center gap-3">
+      <Image
+        src="/assets/images/user_lv1.png"
+        alt="user"
+        width={150}
+        height={150}
+      />
+
+      <UserProfileEdit
+        name={profileData?.name || ''}
+        bio={profileData?.bio || ''}
+        userId={profileData?.id || ''}
+      />
+    </div>
   );
 };
 
