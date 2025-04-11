205e3c8dd6f949cf6a8573d3fe03fc9d97dd3f79
Handsol <imhandsol@gmail.com>
2025-04-11 21:12:19 +0900

    Style: 레이아웃에 사이드바 여백 추가 #86

--- src/app/(main-contents)/layout.tsx
+++ src/app/(main-contents)/layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children, modal }: LayoutProps) => {
   return (
     <main className="w-full flex flex-col items-center">
       {/* 반응형을 위해서 div를 추가 */}
-      <div className="max-w-[1024px] flex flex-row justify-center">
+      <div className="max-w-[1024px] ml-[270px] flex flex-row justify-center">
         <Sidebar />
         {children}
         {modal}
