c593829b6bc71ca201900c3938da17e8e798dda6
miing_gaeng <pem4134@naver.com>
2025-04-12 21:30:43 +0900

    Feat: team 가입 로직 구현 #96

--- src/components/rank/team/normal/NormalTeamRankCard.tsx.tsx
+++ src/components/rank/team/normal/NormalTeamRankCard.tsx.tsx
@@ -7,24 +7,37 @@ import { TeamDetailModal } from '../TeamDetailModalContents';
 import { NormalTeamRankLabel } from './NormalTeamRankLabel';
 import { NormalTeamEmblem } from './NormalTeamEmblem';
 import { NormalTeamInfo } from './NormalTeamInfo';
+import TeamJoin from '@/components/team/TeamJoin';
 
 type Props = {
   team: TeamWithPoints;
   rank: number;
+  hasTeam: boolean;
 };
 
-export const NormalRankTeamCard = ({ team, rank }: Props) => {
+export const NormalRankTeamCard = ({ team, rank, hasTeam }: Props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const currentMembers = team.memberCount;
   return (
     <>
-      <article
-        onClick={() => setIsModalOpen(true)}
-        className="border rounded-3xl p-4 shadow-md bg-gray-100 w-full h-24 flex items-center"
-      >
-        <NormalTeamRankLabel rank={rank} />
-        <NormalTeamEmblem teamName={team.teamName} />
-        <NormalTeamInfo team={team} />
+      <article className="border rounded-3xl p-4 shadow-md bg-gray-100 w-full h-24 flex items-center">
+        {/* 클릭시 모달이 열리는 영역 */}
+        <div
+          onClick={() => {
+            setIsModalOpen(true);
+          }}
+        >
+          <NormalTeamRankLabel rank={rank} />
+          <NormalTeamEmblem teamName={team.teamName} />
+          <NormalTeamInfo team={team} />
+        </div>
+        {/* 팀 가입 버튼 */}
+        <TeamJoin
+          team={team}
+          hasTeam={hasTeam}
+          currentMembers={currentMembers}
+        />
       </article>
 
       <CommonModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
