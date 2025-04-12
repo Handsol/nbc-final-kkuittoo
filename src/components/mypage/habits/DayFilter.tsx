1a6b8651dbbe353f104d8cd246a655acedbff77f
verdantgreeny <verdantgreeny@gmail.com>
2025-04-12 21:56:23 +0900

    Add: DayFilter 컴포넌트 추가 #92

---
+++ src/components/mypage/habits/DayFilter.tsx
@@ -0,0 +1,29 @@
+import { DAYS_OF_WEEK } from '@/constants/habits.constants';
+
+type DayFilterProps = {
+  selectedDay: string | null;
+  setSelectedDay: (day: string | null) => void;
+};
+
+const DayFilter = ({ selectedDay, setSelectedDay }: DayFilterProps) => (
+  <article className="flex">
+    {DAYS_OF_WEEK.map((day) => (
+      <button
+        key={day}
+        onClick={() => setSelectedDay(selectedDay === day ? null : day)}
+        className={`
+            flex-1 pb-2 text-sm transition border-b-4 font-dohyeon
+            ${
+              selectedDay === day
+                ? 'border-main text-main font-semibold'
+                : 'border-light-gray text-dark-gray'
+            }
+          `}
+      >
+        {day.charAt(0).toUpperCase() + day.slice(1)}
+      </button>
+    ))}
+  </article>
+);
+
+export default DayFilter;
