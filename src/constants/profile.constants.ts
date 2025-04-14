66a682a5b5fb7618593b5e4098fd8f0779c43c46
Handsol <imhandsol@gmail.com>
2025-04-14 09:46:43 +0900

    Feat: User 레벨 상수화 #86

--- src/constants/profile.constants.ts
+++ src/constants/profile.constants.ts
@@ -1,3 +1,11 @@
 export const DEFAULT_PROFILE = {
   BIO: '소개가 아직 없어요.',
 };
+
+export const LEVELS = {
+  LV5: 40,
+  LV4: 30,
+  LV3: 20,
+  LV2: 10,
+  LV1: 0,
+};
--- src/lib/utils/user.utils.ts
+++ src/lib/utils/user.utils.ts
@@ -1,7 +1,9 @@
+import { LEVELS } from '@/constants/profile.constants';
+
 export const getUserImageByLevel = (level: number): string => {
-  if (level >= 40) return '/assets/images/user_lv5.png';
-  if (level >= 30) return '/assets/images/user_lv4.png';
-  if (level >= 20) return '/assets/images/user_lv3.png';
-  if (level >= 10) return '/assets/images/user_lv2.png';
+  if (level >= LEVELS.LV5) return '/assets/images/user_lv5.png';
+  if (level >= LEVELS.LV4) return '/assets/images/user_lv4.png';
+  if (level >= LEVELS.LV3) return '/assets/images/user_lv3.png';
+  if (level >= LEVELS.LV2) return '/assets/images/user_lv2.png';
   return '/assets/images/user_lv1.png';
 };
