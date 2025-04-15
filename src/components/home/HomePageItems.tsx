83c03b20d757395fca825ec221e8ae2862164686
Handsol <imhandsol@gmail.com>
2025-04-15 22:14:23 +0900

    Refactor: HomeTitle 컴포넌트를 HomePageItems 컴포넌트로 변경 (페이지네이션 버튼 추가) #103

---
+++ src/components/home/HomePageItems.tsx
@@ -0,0 +1,77 @@
+import { ReactNode } from 'react';
+import { ButtonHTMLAttributes } from 'react';
+
+// 제목과 글자
+type TextProps = {
+  children: string | ReactNode;
+};
+
+export const HomeTitleLg = ({ children }: TextProps) => {
+  return (
+    <p className="font-dohyeon text-xl text-white text-center pt-1">
+      {children}
+    </p>
+  );
+};
+
+export const HomeTitleSm = ({ children }: TextProps) => {
+  return (
+    <p className="font-pretendard text-md text-light-gray text-center">
+      {children}
+    </p>
+  );
+};
+
+// 페이지네이션 앞, 뒤페이지 이동 버튼
+
+type ArrowProps = {
+  direction: 'prev' | 'next';
+  onClick: () => void;
+  icon: React.ReactNode;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const PaginationArrowButton = ({
+  direction,
+  onClick,
+  icon,
+  ...props
+}: ArrowProps) => {
+  return (
+    <button
+      aria-label={direction === 'prev' ? '이전 페이지' : '다음 페이지'}
+      onClick={onClick}
+      className=" text-white hover:text-sub transition"
+      {...props}
+    >
+      {icon}
+    </button>
+  );
+};
+
+// 페이지네이션 숫자 버튼
+
+type NumberProps = {
+  page: number;
+  currentPage: number;
+  onClick: (page: number) => void;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const PaginationNumberButton = ({
+  page,
+  currentPage,
+  onClick,
+  ...props
+}: NumberProps) => {
+  const isActive = page === currentPage;
+
+  return (
+    <button
+      onClick={() => onClick(page)}
+      className={`px-2 py-2 border rounded-xl transition font-medium
+        ${isActive ? 'bg-white text-main' : 'border-white text-white hover:bg-white hover:text-main'}`}
+      {...props}
+    >
+      {page}
+    </button>
+  );
+};
