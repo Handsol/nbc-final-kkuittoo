cd6ef0c8afc3fabb4e8522b7a95c4999da1daa9d
Handsol <imhandsol@gmail.com>
2025-04-16 01:08:00 +0900

    Feat: 로그인 성공 시 토스트 메세지 개선 및 로딩 상태 처리 수정 #103

--- src/components/login/GoogleLogin.tsx
+++ src/components/login/GoogleLogin.tsx
@@ -4,30 +4,24 @@ import { useSession } from 'next-auth/react';
 import { useEffect } from 'react';
 import LoginButton from './LoginButton';
 import { useToast } from '@/lib/hooks/use-toast';
+import ToDashboardButton from './ToDashboardButton';
 
 const GoogleLogin = () => {
   const { data: session, status } = useSession();
   const { toast } = useToast();
 
-  // local storage에 로그인 상태 저장
   useEffect(() => {
     if (status === 'authenticated') {
-      localStorage.setItem('islogined', 'true');
-
-      // 로그인 성공 시 토스트 메세지 표시
-      toast({ description: '로그인 성공' });
-    } else if (status === 'unauthenticated') {
-      localStorage.removeItem('islogined');
+      toast({
+        title: '로그인되었습니다',
+        description: `${session.user.name}님 환영합니다!`,
+      });
     }
   }, [status]);
 
   return (
     <div>
-      {status === 'loading' ? (
-        <div className="h-8 w-8 rounded-full bg-gray-200 animate-pulse"></div>
-      ) : (
-        <LoginButton />
-      )}
+      {status === 'authenticated' ? <ToDashboardButton /> : <LoginButton />}
     </div>
   );
 };
