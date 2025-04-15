206a5a13beb99f85e8c35fee0e1a90bc8db99716
Handsol <imhandsol@gmail.com>
2025-04-15 22:04:47 +0900

    Feat: 페이지네이션 내용이 들어갈 구역 지정 #103

---
+++ src/components/home/HomePagination.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useState } from 'react';
+import HomeBioPage from './HomeBioPage';
+import HomeBioPageItem from './HomeBioPageItem';
+
+const TOTAL_PAGES = 3;
+
+const HomePagination = () => {
+  const [page, setPage] = useState(1);
+
+  return (
+    <article className="flex flex-col items-center gap-4">
+      <section
+        aria-label={`홈페이지 소개 ${page}페이지`}
+        className="w-[700px] h-[500px] border-2 border-white rounded-2xl flex items-center justify-center"
+      >
+        <HomeBioPageItem page={page} />
+      </section>
+
+      <HomeBioPage
+        currentPage={page}
+        setPage={setPage}
+        totalPages={TOTAL_PAGES}
+      />
+    </article>
+  );
+};
+
+export default HomePagination;
