ae049598a7bd3541cf4ac3392fb3e89fcdc79c72
Handsol <imhandsol@gmail.com>
2025-04-16 01:34:41 +0900

    Fix: 사이드바 로고 퀄리티 상향 #103

--- src/components/layout/SidebarLogo.tsx
+++ src/components/layout/SidebarLogo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const SidebarLogo = () => {
   return (
     <Image
-      src="/assets/images/logo_test.png"
+      src="/assets/images/logo_desktop_kkuitddo.png"
       alt="logo"
       width={153}
       height={20}
