f3e69aeeec7db239ec8a409eef70ccadfb536c94
Handsol <imhandsol@gmail.com>
2025-04-15 22:05:48 +0900

    Feat: HomeBioPageItem 컴포넌트 추가 (페이지별 콘텐츠 표시) #103

---
+++ src/components/home/HomeBioPageItem.tsx
@@ -0,0 +1,18 @@
+type HomeBioPageItemProps = {
+  page: number;
+};
+
+const HomeBioPageItem = ({ page }: HomeBioPageItemProps) => {
+  switch (page) {
+    case 1:
+      return <p className="text-white text-lg">1페이지 콘텐츠</p>;
+    case 2:
+      return <p className="text-white text-lg">2페이지 콘텐츠</p>;
+    case 3:
+      return <p className="text-white text-lg">3페이지 콘텐츠</p>;
+    default:
+      return null;
+  }
+};
+
+export default HomeBioPageItem;
