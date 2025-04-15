7ec281eeab0f7e8a9837b483826b6f4e3aa54e4e
Handsol <imhandsol@gmail.com>
2025-04-15 21:31:23 +0900

    Style: HomePage 스타일링 및 서비스 소개 추가 #103

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 import GoogleLogin from '@/components/login/GoogleLogin';
+import HomePageBio from '@/components/home/HomePageBio';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-main min-h-screen flex flex-col items-center justify-center gap-12">
+      <Image
+        alt="로고"
+        src={'/assets/images/logo_desktop_kkuitddo.png'}
+        width={301}
+        height={40}
+      />
+      <HomePageBio />
       <GoogleLogin />
     </div>
   );
