07fa5a2f73f403e1e303772beb8994d567e1c490
bonseung-dev <glynnregister@gmail.com>
2025-04-16 12:08:13 +0900

    Style: 유저 랭킹 페이지 UI 수정 #111

--- src/components/rank/user/UserTopSection.tsx
+++ src/components/rank/user/UserTopSection.tsx
@@ -6,14 +6,19 @@ type Props = {
 };
 
 export const UserTopSection = ({ topUsers }: Props) => {
+  const ordered = [topUsers[1], topUsers[0], topUsers[2]]; // 2,1,3 순서
+
   return (
     <section className="mb-8">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-        {topUsers.map((user, index) => (
-          <div key={user.id}>
-            <UserRankCard user={user} rank={index + 1} isTopRank />
-          </div>
-        ))}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-[52px] items-end">
+        {ordered.map((user) => {
+          const rank = topUsers.indexOf(user) + 1;
+          return (
+            <div key={user.id} className="flex justify-center">
+              <UserRankCard user={user} rank={rank} isTopRank />
+            </div>
+          );
+        })}
       </div>
     </section>
   );
