8f0597edd64eb32677b31a28d9439f39ff777c35
Handsol <imhandsol@gmail.com>
2025-04-16 11:06:58 +0900

    Fix: 이미지 경로를 상수로 변경하여 코드 일관성 향상 #103

--- src/components/home/contents/HomeBioPage3.tsx
+++ src/components/home/contents/HomeBioPage3.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import { BioPage3Info } from './items/BioPageItem';
+import { IMAGE_ASSETS } from '@/constants/assets.contants';
 
 const HomeBioPage3 = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-4">
       <Image
         alt="페이지3이미지"
-        src={'/assets/images/pink.png'}
+        src={IMAGE_ASSETS.SPRITE.PINK}
         width={350}
         height={200}
         className="rounded-2xl"
