cc783a0b529294610b1290d1b12457043bf3727e
Handsol <imhandsol@gmail.com>
2025-04-16 11:17:34 +0900

    Fix: IMAGE_ASSETS를 사용하여 캐릭터 이미지 경로를 동적으로 변경 #103

--- src/components/home/contents/items/BioPageItem.tsx
+++ src/components/home/contents/items/BioPageItem.tsx
@@ -4,6 +4,7 @@ import { PiPlant } from 'react-icons/pi';
 import { HomeTitleLg, HomeTitleSm } from '../../HomePageItems';
 import { RiGamepadLine } from 'react-icons/ri';
 import { LuHandshake } from 'react-icons/lu';
+import { IMAGE_ASSETS } from '@/constants/assets.contants';
 
 // 1페이지 서비스 관련 설명 1
 export const ServiceInfo1 = () => {
@@ -56,22 +57,13 @@ export const ServiceInfo3 = () => {
   );
 };
 
-// 2페이지 캐릭터 성장 관련 이미지
-const CHARACTER_IMAGES = [
-  '/assets/images/user_lv1.png',
-  '/assets/images/user_lv2.png',
-  '/assets/images/user_lv3.png',
-  '/assets/images/user_lv4.png',
-  '/assets/images/user_lv5.png',
-];
-
 export const BioPage2Image = () => {
   return (
     <div className="flex flex-row justify-center items-center h-[200px]">
-      {CHARACTER_IMAGES.map((src, idx) => (
+      {Object.values(IMAGE_ASSETS.USER).map((src, idx) => (
         <div key={idx} className="flex items-center">
           <Image alt={`캐릭터${idx + 1}`} src={src} width={100} height={100} />
-          {idx < CHARACTER_IMAGES.length - 1 && (
+          {idx < Object.values(IMAGE_ASSETS.USER).length - 1 && (
             <FaArrowRight className="text-white" />
           )}
         </div>
