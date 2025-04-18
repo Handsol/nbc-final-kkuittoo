57534df1fc67120e6475b9c0531a24eebc82083e
verdantgreeny <verdantgreeny@gmail.com>
2025-04-18 21:09:41 +0900

    Fix: ActionButton을 React.forwardRef로 래핑해 ref 받을 수 있게 수정 #130

--- src/components/common/button/ActionButton.tsx
+++ src/components/common/button/ActionButton.tsx
@@ -1,5 +1,5 @@
 import { ACTIONBUTTON_MODE } from '@/constants/mode.constants';
-import { ButtonHTMLAttributes } from 'react';
+import { ButtonHTMLAttributes, forwardRef } from 'react';
 
 type ActionButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   mode: string;
@@ -17,82 +17,80 @@ type ActionButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
  *   버튼이름
  * </ActionButton>
  */
-const ActionButton = ({
-  mode,
-  children,
-  disabled = false,
-  ...props
-}: ActionButtonProps) => {
-  const baseStyle =
-    'py-2 font-semibold transition-all duration-200 ease-in-out font-dohyeon text-body-sm';
+const ActionButton = forwardRef<HTMLButtonElement, ActionButtonProps>(
+  ({ mode, children, disabled = false, ...props }, ref) => {
+    const baseStyle =
+      'py-2 font-semibold transition-all duration-200 ease-in-out font-dohyeon text-body-sm';
 
-  let sizeStyle = '';
-  let variantStyle = '';
-  let roundedStyle = '';
+    let sizeStyle = '';
+    let variantStyle = '';
+    let roundedStyle = '';
 
-  switch (mode) {
-    case ACTIONBUTTON_MODE.PRIMARY:
-      sizeStyle = 'w-[250px] h-[45px]';
-      variantStyle = 'bg-main text-white hover:bg-sub-light';
-      roundedStyle = 'rounded-full';
-      break;
-    case ACTIONBUTTON_MODE.SECONDARY:
-      sizeStyle = 'w-[250px] h-[45px]';
-      variantStyle = 'bg-sub text-black hover:bg-sub-light';
-      roundedStyle = 'rounded-full';
-      break;
-    case ACTIONBUTTON_MODE.PRIMARY_SMALL:
-      sizeStyle = 'w-[80px] h-[36px]';
-      variantStyle = 'bg-main text-white hover:opacity-85';
-      roundedStyle = 'rounded-full';
-      break;
-    case ACTIONBUTTON_MODE.SECONDARY_SMALL:
-      sizeStyle = 'w-[80px] h-[36px]';
-      variantStyle = 'bg-sub text-black hover:opacity-85';
-      roundedStyle = 'rounded-full';
-      break;
-    case ACTIONBUTTON_MODE.DARK_GRAY_SMALL:
-      sizeStyle = 'w-[80px] h-[36px]';
-      variantStyle = 'bg-dark-gray text-white border hover:bg-medium-gray';
-      roundedStyle = 'rounded-full';
-      break;
-    case ACTIONBUTTON_MODE.ROUNDED_MD:
-      sizeStyle = 'w-[180px] h-[40px]';
-      variantStyle = 'bg-main text-white hover:bg-sub';
-      roundedStyle = 'rounded-md';
-      break;
-    case ACTIONBUTTON_MODE.ROUNDED_MD_LIGHT_GRAY:
-      sizeStyle = 'w-[180px] h-[40px]';
-      variantStyle =
-        'bg-light-gray text-black hover:bg-medium-gray hover:text-white';
-      roundedStyle = 'rounded-md';
-      break;
-    case ACTIONBUTTON_MODE.LOGOUT:
-      sizeStyle = 'px-5 text-body-lg w-full';
-      variantStyle =
-        'text-black border border-transparent hover:text-white hover:bg-medium-gray bg-light-gray';
-      roundedStyle = 'rounded-md';
-      break;
-    default:
-      sizeStyle = 'w-[250px] h-[45px] text-body-md';
-      variantStyle = 'bg-main text-white hover:bg-sub';
-      roundedStyle = 'rounded-full';
-      break;
-  }
+    switch (mode) {
+      case ACTIONBUTTON_MODE.PRIMARY:
+        sizeStyle = 'w-[250px] h-[45px]';
+        variantStyle = 'bg-main text-white hover:bg-sub-light';
+        roundedStyle = 'rounded-full';
+        break;
+      case ACTIONBUTTON_MODE.SECONDARY:
+        sizeStyle = 'w-[250px] h-[45px]';
+        variantStyle = 'bg-sub text-black hover:bg-sub-light';
+        roundedStyle = 'rounded-full';
+        break;
+      case ACTIONBUTTON_MODE.PRIMARY_SMALL:
+        sizeStyle = 'w-[80px] h-[36px]';
+        variantStyle = 'bg-main text-white hover:opacity-85';
+        roundedStyle = 'rounded-full';
+        break;
+      case ACTIONBUTTON_MODE.SECONDARY_SMALL:
+        sizeStyle = 'w-[80px] h-[36px]';
+        variantStyle = 'bg-sub text-black hover:opacity-85';
+        roundedStyle = 'rounded-full';
+        break;
+      case ACTIONBUTTON_MODE.DARK_GRAY_SMALL:
+        sizeStyle = 'w-[80px] h-[36px]';
+        variantStyle = 'bg-dark-gray text-white border hover:bg-medium-gray';
+        roundedStyle = 'rounded-full';
+        break;
+      case ACTIONBUTTON_MODE.ROUNDED_MD:
+        sizeStyle = 'w-[180px] h-[40px]';
+        variantStyle = 'bg-main text-white hover:bg-sub';
+        roundedStyle = 'rounded-md';
+        break;
+      case ACTIONBUTTON_MODE.ROUNDED_MD_LIGHT_GRAY:
+        sizeStyle = 'w-[180px] h-[40px]';
+        variantStyle =
+          'bg-light-gray text-black hover:bg-medium-gray hover:text-white';
+        roundedStyle = 'rounded-md';
+        break;
+      case ACTIONBUTTON_MODE.LOGOUT:
+        sizeStyle = 'px-5 text-body-lg w-full';
+        variantStyle =
+          'text-black border border-transparent hover:text-white hover:bg-medium-gray bg-light-gray';
+        roundedStyle = 'rounded-md';
+        break;
+      default:
+        sizeStyle = 'w-[250px] h-[45px] text-body-md';
+        variantStyle = 'bg-main text-white hover:bg-sub';
+        roundedStyle = 'rounded-full';
+        break;
+    }
 
-  const disabledStyle = disabled
-    ? `bg-light-gray text-medium-gray cursor-not-allowed ${roundedStyle}`
-    : `${variantStyle} ${roundedStyle}`;
-  return (
-    <button
-      type={props.type || 'button'}
-      disabled={disabled}
-      className={`${baseStyle} ${sizeStyle} ${disabledStyle}`}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-};
+    const disabledStyle = disabled
+      ? `bg-light-gray text-medium-gray cursor-not-allowed ${roundedStyle}`
+      : `${variantStyle} ${roundedStyle}`;
+    return (
+      <button
+        type={props.type || 'button'}
+        disabled={disabled}
+        className={`${baseStyle} ${sizeStyle} ${disabledStyle}`}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  },
+);
 
 export default ActionButton;
