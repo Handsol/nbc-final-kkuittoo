732ef23f5a90db9d57ab28a1c4fcdfbad52ff1d4
verdantgreeny <verdantgreeny@gmail.com>
2025-04-21 23:27:17 +0900

    Style: 카테고리 필터 스타일 변경 #133

--- src/components/mypage/habits/habit-filter/MobileCategoryFilter.tsx
+++ src/components/mypage/habits/habit-filter/MobileCategoryFilter.tsx
@@ -3,6 +3,7 @@ import {
   HABIT_CATEGORY_LABELS,
 } from '@/constants/habits.constants';
 import { Categories } from '@prisma/client';
+import { FaCircleChevronDown } from 'react-icons/fa6';
 
 type MobileCategoryFilterProps = {
   selectedCategory: Categories | null;
@@ -19,19 +20,25 @@ const MobileCategoryFilter = ({
   };
 
   return (
-    <select
-      onChange={handleChange}
-      value={selectedCategory || 'ALL'}
-      className="w-[76px] p-2 border border-medium-gray rounded-lg"
-      aria-label="Category filter"
-    >
-      <option value="ALL">ALL</option>
-      {HABIT_CATEGORIES.map((c) => (
-        <option key={c} value={c}>
-          {HABIT_CATEGORY_LABELS[c]}
-        </option>
-      ))}
-    </select>
+    <div className="relative min-w-[100px] max-w-full">
+      <select
+        onChange={handleChange}
+        value={selectedCategory || 'ALL'}
+        className="w-full appearance-none pl-[16px] pr-[36px] py-[8px] text-body-md border border-medium-gray rounded-lg bg-white text-gray-700 focus:outline-none focus:ring-2 focus:ring-main"
+        aria-label="Category filter"
+      >
+        <option value="ALL">ALL</option>
+        {HABIT_CATEGORIES.map((c) => (
+          <option key={c} value={c}>
+            {HABIT_CATEGORY_LABELS[c]}
+          </option>
+        ))}
+      </select>
+
+      <div className="pointer-events-none absolute inset-y-0 right-[12px] flex items-center text-main">
+        <FaCircleChevronDown className="w-5 h-5" />
+      </div>
+    </div>
   );
 };
 
