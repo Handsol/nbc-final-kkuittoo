07ba83b0a11f26dadd300259fe55704278527c5c
Handsol <imhandsol@gmail.com>
2025-04-24 01:42:00 +0900

    Fix: AppliedButton 및 ApplyButton 컴포넌트에서 불필요한 border 클래스 제거 #136

--- src/components/shop/items/ApplyButton.tsx
+++ src/components/shop/items/ApplyButton.tsx
@@ -1,7 +1,7 @@
 const ApplyButton = () => {
   return (
     <>
-      <button className="w-[75px] h-[36px] bg-white text-main border border-main border-2 rounded-md hover:bg-sub ">
+      <button className="w-[75px] h-[36px] bg-white text-main border-2 border-main rounded-md hover:bg-sub ">
         <p className="font-dohyeon text-body-sm text-main">Apply</p>
       </button>
     </>
