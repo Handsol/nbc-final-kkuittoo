31cc65b7cb8be0f06c382a5fddb4193297b12811
verdantgreeny <verdantgreeny@gmail.com>
2025-04-23 10:02:06 +0900

    Fix: NoTeam 모바일 반응형 수정 #143

--- src/components/dashboard/NoTeam.tsx
+++ src/components/dashboard/NoTeam.tsx
@@ -8,21 +8,26 @@ import { IMAGE_ASSETS } from '@/constants/assets.contants';
 
 const NoTeam = () => {
   return (
-    <section className="relative w-full p-6 flex flex-col md:flex-row items-center md:items-start gap-6 bg-sub-light mb-10">
+    <section className="relative w-full p-4 md:p-6 flex flex-col md:flex-row items-center md:items-start gap-4 md:gap-6 bg-sub-light">
       {/* 기본 이미지 */}
-      <Image
-        src={IMAGE_ASSETS.SPRITE.BLUE}
-        alt="Guild Image"
-        width={200}
-        height={100}
-        className="rounded-lg hidden md:block"
-      />
+      <div className="flex-shrink-0">
+        <Image
+          src={IMAGE_ASSETS.SPRITE.BLUE}
+          alt="Guild Image"
+          width={120}
+          height={60}
+          className="rounded-lg w-32 h-auto md:w-64"
+        />
+      </div>
       {/* 메인 영역 */}
-      <article className="flex flex-col items-center md:items-start justify-center gap-3 text-left">
+      <article className="flex flex-col items-center md:items-start justify-center gap-2 md:gap-3 text-left flex-1">
         {/* 제목 */}
-        <Title mode={TITLE_MODE.SECTION_TITLE}>FIND OR MAKE MY TEAM</Title>
+        <div className="hidden md:block md:text-body-md">
+          <Title mode={TITLE_MODE.SECTION_TITLE}>FIND OR MAKE MY TEAM</Title>
+        </div>
+
         {/* 팀을 찾거나 팀을 생성하는 영역 */}
-        <div className="w-full flex flex-col justify-between text-sm gap-4">
+        <div className="w-full flex flex-col justify-between text-body-xs md:text-body-sm gap-2 md:gap-4">
           <Text>
             <span className="font-bold">
               새로운 팀을 찾거나 직접 팀을 만들어 보세요!
@@ -32,7 +37,7 @@ const NoTeam = () => {
             합류해 몬스터를 격파하는 짜릿한 협동전의 주인공이 되어보세요!
           </Text>
           {/* 버튼 wrapper */}
-          <div className="flex flex-row gap-4 w-full mt-2 items-center justify-center md:items-start md:justify-normal">
+          <div className="flex flex-row flex-wrap gap-2 md:gap-4 w-full mt-1 md:mt-2 items-center justify-center md:items-start md:justify-normal">
             <LinkButton mode={LINKBUTTON_MODE.COMMON} href={PATH.RANK.TEAMS}>
               Find New Team
             </LinkButton>
