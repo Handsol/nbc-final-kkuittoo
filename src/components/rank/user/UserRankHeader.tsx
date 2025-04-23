6a21b58995768592d1a6b98de012fc6c158d9f0f
bonseung-dev <glynnregister@gmail.com>
2025-04-23 12:17:43 +0900

    Style: 반응형 Tab UI 구현 #144

--- src/components/rank/user/UserRankHeader.tsx
+++ src/components/rank/user/UserRankHeader.tsx
@@ -1,20 +1,33 @@
+'use client';
+
 import LinkButton from '@/components/common/button/LinkButton';
-import MobileTabButton from '@/components/common/button/MobileTabButton';
 import { LINKBUTTON_MODE } from '@/constants/mode.constants';
 import { PATH } from '@/constants/path.constants';
 import { SearchInput } from '../SearchInput';
+import Link from 'next/link';
+import { getTabButtonClass } from '@/styles/tabButtonStyles';
+import { usePathname } from 'next/navigation';
 
 export const UserRankHeader = () => {
+  const pathname = usePathname();
   return (
-    <section className="flex flex-col md:flex-row md:items-center md:justify-between mb-8 mt-[54px] gap-2 md:gap-0">
-      {/* ✅ 모바일 탭 버튼 */}
+    <section className="flex flex-col md:flex-row md:items-center md:justify-between mb-8 gap-2 md:gap-0">
+      {/* 모바일 탭 버튼*/}
       <div className="flex w-full md:hidden">
-        <MobileTabButton href={PATH.RANK.USERS} label="Character" />
-        <MobileTabButton href={PATH.RANK.TEAMS} label="Team" />
+        <Link href={PATH.RANK.USERS} className="w-full">
+          <button className={getTabButtonClass(pathname === PATH.RANK.USERS)}>
+            Character
+          </button>
+        </Link>
+        <Link href={PATH.RANK.TEAMS} className="w-full">
+          <button className={getTabButtonClass(pathname === PATH.RANK.TEAMS)}>
+            Team
+          </button>
+        </Link>
       </div>
 
-      {/* ✅ PC용 LinkButton */}
-      <div className="hidden md:flex space-x-2">
+      {/* PC용 LinkButton */}
+      <div className="hidden md:flex space-x-2 p-4">
         <LinkButton mode={LINKBUTTON_MODE.RANK} href={PATH.RANK.USERS} disabled>
           Character
         </LinkButton>
