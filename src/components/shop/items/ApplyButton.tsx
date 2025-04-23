a157493078e96b1f0765e38b17e4e63e45242d85
Handsol <imhandsol@gmail.com>
2025-04-24 01:40:59 +0900

    Feat: ApplyButton 컴포넌트 추가 #136

---
+++ src/components/shop/items/ApplyButton.tsx
@@ -0,0 +1,11 @@
+const ApplyButton = () => {
+  return (
+    <>
+      <button className="w-[75px] h-[36px] bg-white text-main border border-main border-2 rounded-md hover:bg-sub ">
+        <p className="font-dohyeon text-body-sm text-main">Apply</p>
+      </button>
+    </>
+  );
+};
+
+export default ApplyButton;
