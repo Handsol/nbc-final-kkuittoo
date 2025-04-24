f49851b5fb0d610a11c4aea6ced2ff15c39ef264
Handsol <imhandsol@gmail.com>
2025-04-24 20:41:22 +0900

    Feat: Apply, Applied 버튼에 onClick 속성 부여 #136

--- src/components/shop/items/AppliedButton.tsx
+++ src/components/shop/items/AppliedButton.tsx
@@ -1,13 +1,18 @@
 import { FaCheck } from 'react-icons/fa6';
 
-const AppliedButton = () => {
+type AppliedButtonProps = {
+  onClick: () => void;
+};
+
+const AppliedButton = ({ onClick }: AppliedButtonProps) => {
   return (
-    <>
-      <button className="flex flex-row items-center justify-center gap-2 w-[115px] h-[36px] bg-sub-light text-main border-2 border-main rounded-md hover:bg-sub ">
-        <FaCheck className="w-[15px] h-[15px] text-main" />
-        <p className="font-dohyeon text-body-sm text-main">Applied</p>
-      </button>
-    </>
+    <button
+      onClick={onClick}
+      className="flex flex-row items-center justify-center gap-2 w-[115px] h-[36px] bg-sub-light text-main border-2 border-main rounded-md hover:bg-sub "
+    >
+      <FaCheck className="w-[15px] h-[15px] text-main" />
+      <p className="font-dohyeon text-body-sm text-main">Applied</p>
+    </button>
   );
 };
 
--- src/components/shop/items/ApplyButton.tsx
+++ src/components/shop/items/ApplyButton.tsx
@@ -1,10 +1,15 @@
-const ApplyButton = () => {
+type ApplyButtonProps = {
+  onClick: () => void;
+};
+
+const ApplyButton = ({ onClick }: ApplyButtonProps) => {
   return (
-    <>
-      <button className="w-[75px] h-[36px] bg-white text-main border-2 border-main rounded-md hover:bg-sub ">
-        <p className="font-dohyeon text-body-sm text-main">Apply</p>
-      </button>
-    </>
+    <button
+      onClick={onClick}
+      className="w-[75px] h-[36px] bg-white text-main border-2 border-main rounded-md hover:bg-sub "
+    >
+      <p className="font-dohyeon text-body-sm text-main">Apply</p>
+    </button>
   );
 };
 
