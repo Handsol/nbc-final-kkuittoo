92e531fed01de496bce4a492a8e23a80a0bd995f
bonseung-dev <glynnregister@gmail.com>
2025-04-24 09:35:15 +0900

    Style: 멤버리스트 반응형 UI 적용 #156

---
+++ src/components/team/TeamMemberAvatar.tsx
@@ -0,0 +1,19 @@
+import { getUserImageByLevel } from '@/lib/utils/user.utils';
+import Image from 'next/image';
+
+type Props = { userName?: string | null; level: number };
+
+export const TeamMemberAvatar = ({ userName, level }: Props) => {
+  const imageUrl = getUserImageByLevel(level);
+  return (
+    <figure className="w-40 h-40 md:w-16 md:h-16 bg-sub-light flex items-center justify-center mb-2 overflow-hidden shrink-0 relative">
+      <Image
+        src={imageUrl ?? '/images/default.png'}
+        alt={userName ?? '유저 아바타'}
+        width={128}
+        height={128}
+        className="object-cover rounded-lg w-full h-full"
+      />
+    </figure>
+  );
+};
