9408be675f5b212ce18b07eb5b0c4c5b2701fa3b
Handsol <imhandsol@gmail.com>
2025-04-24 16:42:06 +0900

    Feat: 구매한 목록이 나타날 아이템리스트 컴포넌트 생성 #136

---
+++ src/components/shop/PurchasedItemList.tsx
@@ -0,0 +1,30 @@
+import { ItemList } from '@/types/shop.type';
+import Title from '../common/Title';
+import { TITLE_MODE } from '@/constants/mode.constants';
+import PurchasedItemCard from './PurchasedItemCard';
+
+type PurchasedItemListProps = {
+  itemList: ItemList;
+};
+
+const PurchasedItemList = ({ itemList }: PurchasedItemListProps) => {
+  const itemNumber = itemList.length;
+
+  return (
+    <article>
+      <Title
+        mode={TITLE_MODE.SECTION_SUBTITLE}
+        className="text-body-md text-medium-gray"
+      >
+        {`Purchased Items (${itemNumber})`}
+      </Title>
+      <section className="w-full grid grid-cols-1 md:grid-cols-2 mt-[24px] gap-[24px]">
+        {itemList.map((item) => (
+          <PurchasedItemCard key={item.id} item={item} />
+        ))}
+      </section>
+    </article>
+  );
+};
+
+export default PurchasedItemList;
