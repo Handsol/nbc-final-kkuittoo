f1f8d266e6d22d8d9d7586977685986a29eba6c1
miing_gaeng <pem4134@naver.com>
2025-04-25 20:35:57 +0900

    Feat: 팀 가입여부/해당 팀멤버 여부에 따라 버튼 분기처리 #166

--- src/components/team/TeamMemberList.tsx
+++ src/components/team/TeamMemberList.tsx
@@ -1,26 +1,50 @@
 import TeamMemberCard from './TeamMemberCard';
 import Title from '../common/Title';
-import { TITLE_MODE } from '@/constants/mode.constants';
+import { JOINBUTTON_MODE, TITLE_MODE } from '@/constants/mode.constants';
 import { getUserLevel } from '@/lib/utils/user-level.utils';
-import { fetchGetTeamMembers } from '@/lib/services/team-actions.services';
+import {
+  fetchGetTeamData,
+  fetchGetTeamMembers,
+  fetchGetUserTeamInfo,
+} from '@/lib/services/team-actions.services';
 import TeamLeave from './TeamLeave';
 import TeamInviteButton from './team-join/TeamInviteButton';
+import { getUserSession } from '@/lib/services/getUserSession.services';
+import UnauthorizedPage from '../loading-error-page/UnauthorizedPage';
+import TeamJoin from './TeamJoin';
+import { TeamData } from '@/types/teams.type';
 
 type TeamMemberListProps = {
   id: string;
 };
 
 const TeamMemberList = async ({ id }: TeamMemberListProps) => {
+  const session = await getUserSession();
+  if (!session) return <UnauthorizedPage />;
+  const userId = session.user.id;
+
+  const teamData = await fetchGetTeamData(id);
   const teamMemberList = await fetchGetTeamMembers(id);
+  const { isThisTeamMember, isUserhasTeam, currentTeamMembers } =
+    await fetchGetUserTeamInfo(userId, id);
 
   return (
     <div className="w-full flex flex-col gap-5 mt-11 mb-11">
       <div className="w-full flex justify-between items-center">
         <Title mode={TITLE_MODE.SECTION_TITLE}>팀 멤버</Title>
-        <div className="flex gap-4">
-          <TeamInviteButton id={id} />
-          <TeamLeave id={id} />
-        </div>
+        {isThisTeamMember ? (
+          <div className="flex gap-4">
+            <TeamInviteButton id={id} />
+            <TeamLeave id={id} />
+          </div>
+        ) : (
+          <TeamJoin
+            team={teamData}
+            hasTeam={isUserhasTeam}
+            currentMembers={currentTeamMembers as number}
+            mode={JOINBUTTON_MODE.TEAM_PAGE}
+          />
+        )}
       </div>
       <ul className="w-full flex flex-col gap-2">
         {teamMemberList.map((member, index) => {
