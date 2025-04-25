6e030ee84cc7f1a85c0ff93d97d199604a3c68f6
miing_gaeng <pem4134@naver.com>
2025-04-25 16:17:53 +0900

    Style: 구글 로고 수정 #166

--- src/components/login/LoginButton.tsx
+++ src/components/login/LoginButton.tsx
@@ -2,7 +2,7 @@
 
 import { PATH } from '@/constants/path.constants';
 import { signIn } from 'next-auth/react';
-import { FcGoogle } from 'react-icons/fc';
+import { FaGoogle } from 'react-icons/fa';
 import Title from '../common/Title';
 import { TITLE_MODE } from '@/constants/mode.constants';
 
@@ -16,7 +16,7 @@ const LoginButton = () => {
       onClick={handleLogin}
       className="flex flex-row justify-center items-center text-center w-[250px] h-[55px] rounded-full bg-main gap-[10px]"
     >
-      <FcGoogle className="w-10 h-10 bg-white rounded-full" />
+      <FaGoogle className="text-white" />
       <div className="pt-1">
         <Title mode={TITLE_MODE.LINK} className="text-white">
           구글로 시작하기
