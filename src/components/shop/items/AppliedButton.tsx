39761417dd74bf567e54183cc46ff84b63e53e54
Handsol <imhandsol@gmail.com>
2025-04-26 00:09:22 +0900

    Feat: ApplyButton 및 AppliedButton에서 button 태그를 ActionButton 컴포넌트로 변경 #136

--- src/components/shop/items/AppliedButton.tsx
+++ src/components/shop/items/AppliedButton.tsx
@@ -1,3 +1,5 @@
+import ActionButton from '@/components/common/button/ActionButton';
+import { ACTIONBUTTON_MODE } from '@/constants/mode.constants';
 import { FaCheck } from 'react-icons/fa6';
 
 type AppliedProps = {
@@ -6,13 +8,10 @@ type AppliedProps = {
 
 const AppliedButton = ({ onClick }: AppliedProps) => {
   return (
-    <button
-      onClick={onClick}
-      className="flex flex-row items-center justify-center gap-2 w-[115px] h-[36px] bg-sub-light text-main border-2 border-main rounded-md hover:bg-sub "
-    >
+    <ActionButton mode={ACTIONBUTTON_MODE.ROUNDED_MD_APPLIED} onClick={onClick}>
       <FaCheck className="w-[15px] h-[15px] text-main" />
       <p className="font-dohyeon text-body-sm text-main">적용됨</p>
-    </button>
+    </ActionButton>
   );
 };
 
--- src/components/shop/items/ApplyButton.tsx
+++ src/components/shop/items/ApplyButton.tsx
@@ -1,15 +1,15 @@
+import ActionButton from '@/components/common/button/ActionButton';
+import { ACTIONBUTTON_MODE } from '@/constants/mode.constants';
+
 type ApplyProps = {
   onClick: () => void;
 };
 
 const ApplyButton = ({ onClick }: ApplyProps) => {
   return (
-    <button
-      className="w-[100px] h-[36px] bg-white text-main border-2 border-main rounded-md hover:bg-sub"
-      onClick={onClick}
-    >
+    <ActionButton mode={ACTIONBUTTON_MODE.ROUNDED_MD_APPLY} onClick={onClick}>
       <p className="font-dohyeon text-body-sm text-main">적용하기</p>
-    </button>
+    </ActionButton>
   );
 };
 
