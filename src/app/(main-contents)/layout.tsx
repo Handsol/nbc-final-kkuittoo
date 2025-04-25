bb3eff5e37b70226f481a012e7ec745b828bad17
miing_gaeng <pem4134@naver.com>
2025-04-25 16:59:50 +0900

    Feat: 카카오 공유하기 SDK 추가 #166

--- src/app/(main-contents)/layout.tsx
+++ src/app/(main-contents)/layout.tsx
@@ -1,5 +1,6 @@
 import Sidebar from '@/components/layout/Sidebar';
 import SidebarMobileWrapper from '@/components/layout/SidebarMobileWrapper';
+import Script from 'next/script';
 import { ReactNode } from 'react';
 
 type LayoutProps = {
@@ -9,22 +10,29 @@ type LayoutProps = {
 
 const Layout = ({ children, modal }: LayoutProps) => {
   return (
-    <main className="w-full flex flex-col items-center">
-      <div className="w-full max-w-[1440px] flex flex-col md:flex-row justify-center">
-        {/* 데스크탑 사이드바 */}
-        <div className="hidden md:block">
-          <Sidebar />
+    <>
+      {/* 카카오 공유하기 SDK */}
+      <Script
+        id="kakao-sdk"
+        src="https://developers.kakao.com/sdk/js/kakao.js"
+        strategy="beforeInteractive"
+      />
+
+      <main className="w-full flex flex-col items-center">
+        <div className="w-full max-w-[1440px] flex flex-col md:flex-row justify-center">
+          {/* 데스크탑 사이드바 */}
+          <div className="hidden md:block">
+            <Sidebar />
+          </div>
+          {/* 모바일용 슬라이드 사이드바 & 헤더 */}
+          <SidebarMobileWrapper />
+          <article className="w-full">
+            {children}
+            {modal}
+          </article>
         </div>
-
-        {/* 모바일용 슬라이드 사이드바 & 헤더 */}
-        <SidebarMobileWrapper />
-
-        <article className="w-full">
-          {children}
-          {modal}
-        </article>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
