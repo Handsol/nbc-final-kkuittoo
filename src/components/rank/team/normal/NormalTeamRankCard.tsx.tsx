4ee2040f1a83f4b16cd0031d45d82e9f62bbe38d
bonseung-dev <glynnregister@gmail.com>
2025-04-27 19:55:45 +0900

    Feat: 팀 랭킹 공개/비공개 아이콘 구현 #162

--- src/components/rank/team/normal/NormalTeamRankCard.tsx.tsx
+++ src/components/rank/team/normal/NormalTeamRankCard.tsx.tsx
@@ -5,6 +5,7 @@ import { NormalTeamRankLabel } from './NormalTeamRankLabel';
 import { NormalTeamEmblem } from './NormalTeamEmblem';
 import { NormalTeamInfo } from './NormalTeamInfo';
 import TeamJoin from '@/components/team/TeamJoin';
+import TeamOpenNotEditMode from '@/components/team/team-edit/TeamOpenNotEditMode';
 
 type Props = {
   team: TeamWithPoints;
@@ -20,17 +21,23 @@ export const NormalRankTeamCard = ({ team, rank, hasTeam }: Props) => {
         {/* 왼쪽 정보 그룹 */}
         <div className="flex items-center gap-3 flex-1 min-w-0">
           <NormalTeamRankLabel rank={rank} />
-          <NormalTeamEmblem teamName={team.teamName} embluem={team.emblem} />
+          <div className="hidden md:block">
+            <NormalTeamEmblem teamName={team.teamName} embluem={team.emblem} />
+          </div>
           <NormalTeamInfo team={team} />
         </div>
 
         {/* 오른쪽 버튼 영역 */}
-        <div className="shrink-0 ml-2">
-          <TeamJoin
-            team={team}
-            hasTeam={hasTeam}
-            currentMembers={currentMembers}
-          />
+        <div className="flex items-center gap-2 shrink-0 ml-2">
+          {hasTeam === false && (
+            <TeamJoin
+              team={team}
+              hasTeam={hasTeam}
+              currentMembers={currentMembers}
+            />
+          )}
+          {/* 항상 아이콘은 존재 */}
+          <TeamOpenNotEditMode isOpened={team.isOpened} />
         </div>
       </article>
     </>
