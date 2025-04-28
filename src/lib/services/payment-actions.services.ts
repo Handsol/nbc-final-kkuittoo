d52888a871974255a545d6f4723f9f5341b3e791
Handsol <imhandsol@gmail.com>
2025-04-28 16:02:33 +0900

    Feat: 아이템 적용 시 isApplied 상태를 true로 변경하는 함수 추가 #177

--- src/lib/services/payment-actions.services.ts
+++ src/lib/services/payment-actions.services.ts
@@ -40,3 +40,23 @@ export const fetchGetItemList = async (userId: string) => {
     throw new Error(`${SHOP_MESSAGE.ITEM.FETCH_FAIL}, itemList`);
   }
 };
+
+// 아이템 적용 시 isApplied 상태를 true로 변경
+export const fetchPatchApplyItem = async (userItemId: string) => {
+  try {
+    const res = await fetch(`/api/user-items/${userItemId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ isApplied: true }),
+    });
+    if (!res.ok) {
+      throw new Error(`Failed to apply item with ID: ${userItemId}`);
+    }
+    return res.json();
+  } catch (error) {
+    console.error(error);
+    throw new Error(`Failed to apply item: ${error}`);
+  }
+};
