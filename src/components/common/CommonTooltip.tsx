f370557e2d5c949f90f30c5e60643f9e40f26792
verdantgreeny <verdantgreeny@gmail.com>
2025-04-28 16:56:54 +0900

    Fix: commonTooltip ref로 전달 가능하게 수정 #172

--- src/components/common/CommonTooltip.tsx
+++ src/components/common/CommonTooltip.tsx
@@ -7,6 +7,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '../ui/tooltip';
+import { forwardRef } from 'react';
 
 type CommonTooltipProps = {
   children: React.ReactNode;
@@ -21,24 +22,26 @@ type CommonTooltipProps = {
  * @param message {string} : TooltipContent에 들어갈 message (실질적으로 뜨는 메세지)
  * !message는 tooltip-message.constants.tsx에 정의된 상수로 관리
  */
-const CommonTooltip = ({
-  children,
-  message,
-  isDisabled,
-}: CommonTooltipProps) => {
-  if (isDisabled) return <>{children}</>;
+const CommonTooltip = forwardRef<HTMLButtonElement, CommonTooltipProps>(
+  ({ children, message, isDisabled }, ref) => {
+    if (isDisabled) return <>{children}</>;
 
-  return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>{children}</TooltipTrigger>
-        <TooltipContent>
-          <p>{message}</p>
-          <TooltipArrow className="fill-sub" />
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-  );
-};
+    return (
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild ref={ref}>
+            {children}
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>{message}</p>
+            <TooltipArrow className="fill-sub" />
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    );
+  },
+);
+
+CommonTooltip.displayName = 'CommonTooltip';
 
 export default CommonTooltip;
