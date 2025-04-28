ec4635cc5de723c8e609f4005b6f85bbe513c2d5
Handsol <imhandsol@gmail.com>
2025-04-28 22:49:48 +0900

    Refactor: UserProfileImage 컴포넌트의 스타일 및 구조 개선 #170

--- src/components/common/UserProfileImage.tsx
+++ src/components/common/UserProfileImage.tsx
@@ -1,23 +1,20 @@
 'use client';
 
 import { usePurchasedItemsQuery } from '@/lib/queries/usePurchasedItemsQuery';
-import { cn } from '@/lib/utils';
 import { getUserImageByLevel } from '@/lib/utils/user.utils';
 import { ShopItem } from '@/types/shop.type';
-
 import Image from 'next/image';
 
 type UserProfileImageProps = {
   level: number;
   size?: 'sm' | 'md' | 'lg' | 'xl';
-  item: string;
 };
 
 const SIZE_VARIANTS = {
-  sm: 'w-[64px] h-[64px]',
-  md: 'w-[112px] h-[112px]',
-  lg: 'w-[128px] h-[128px]',
-  xl: 'w-[150px] h-[150px]',
+  sm: 64,
+  md: 112,
+  lg: 128,
+  xl: 150,
 };
 
 const UserProfileImage = ({ level, size = 'md' }: UserProfileImageProps) => {
@@ -25,13 +22,13 @@ const UserProfileImage = ({ level, size = 'md' }: UserProfileImageProps) => {
   const userImageSrc = getUserImageByLevel(level);
   const appliedItem = data?.find((item) => item.userItems?.[0]?.isApplied);
   const borderImageSrc = appliedItem?.itemImage || null;
+  const profileSize = SIZE_VARIANTS[size];
+  const borderSize = profileSize + 16;
 
   return (
     <div
-      className={cn(
-        'relative flex items-center justify-center',
-        SIZE_VARIANTS[size],
-      )}
+      className="relative flex items-center justify-center"
+      style={{ width: borderSize, height: borderSize }}
     >
       {/* 테두리 */}
       {borderImageSrc && (
@@ -43,12 +40,17 @@ const UserProfileImage = ({ level, size = 'md' }: UserProfileImageProps) => {
         />
       )}
       {/* 프로필 캐릭터 */}
-      <Image
-        src={userImageSrc}
-        alt="user profile"
-        fill
-        className="rounded-full object-cover p-5"
-      />
+      <div
+        className="absolute rounded-full overflow-hidden"
+        style={{ width: profileSize, height: profileSize }}
+      >
+        <Image
+          src={userImageSrc}
+          alt="user profile"
+          fill
+          className="object-cover p-5"
+        />
+      </div>
     </div>
   );
 };
