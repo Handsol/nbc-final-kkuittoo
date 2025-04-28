3082176894da40ccaa6a472196a84b38d13790f2
bonseung-dev <glynnregister@gmail.com>
2025-04-28 23:01:38 +0900

    Fix: CommonModal 수정 #176

--- src/components/common/CommonModal.tsx
+++ src/components/common/CommonModal.tsx
@@ -1,27 +1,9 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 import { MdClose } from 'react-icons/md';
-/**
- * 공통 모달 컴포넌트입니다.
- *
- * 화면 중앙에 모달을 띄우며, 외부 영역(배경)을 클릭하면 닫히도록 설정했습니다.
- * 모달 내부를 클릭하면 닫히지 않도록 클릭 이벤트를 주었습니다.
- *
- * @component
- *
- * @param {boolean} isOpen - 기본값은 `false`이며, `true`일 때만 모달이 보입니다.
- * @param {() => void} onClose - 모달을 닫을 때 실행할 함수입니다. (예: 상태 초기화)
- * @param {ReactNode} children - 모달 내부에 렌더링할 콘텐츠입니다. (예: 상세 정보, 폼 등)
- * @param {'common' | 'parallel'} mode - 모달 모드 분기 (기본: common)
- * @example
- *
- * ```tsx
- * <CommonModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
- *   <p>모달 내용</p>
- * </CommonModal>
- * ```
- */
+
 type CommonModalProps = {
   isOpen: boolean;
   onClose: () => void;
@@ -30,16 +12,21 @@ type CommonModalProps = {
 };
 
 export const CommonModal = ({
-  isOpen = false, // 기본값 설정
+  isOpen,
   onClose,
   children,
-  mode = 'common',
 }: CommonModalProps) => {
-  if (!isOpen) return null;
+  const [mounted, setMounted] = useState(false);
 
-  return (
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!isOpen || !mounted) return null;
+
+  return createPortal(
     <div
-      className="fixed inset-0 z-50 bg-black bg-opacity-60 flex justify-center items-center px-4"
+      className="fixed inset-0 z-[9999] bg-black bg-opacity-80 flex justify-center items-center px-4 isolate"
       onClick={onClose}
     >
       <div
@@ -56,6 +43,7 @@ export const CommonModal = ({
         </button>
         {children}
       </div>
-    </div>
+    </div>,
+    document.body,
   );
 };
