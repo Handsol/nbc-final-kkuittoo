33abb7007e576b86d146160ba81d112d61b45014
Handsol <imhandsol@gmail.com>
2025-04-28 21:04:12 +0900

    Fix: 불필요한 이미지 가져오기 및 관련 코드 제거 #177

--- src/components/layout/SidebarProfile.tsx
+++ src/components/layout/SidebarProfile.tsx
@@ -2,10 +2,8 @@
 
 import { useSession } from 'next-auth/react';
 import { useUserQuery } from '@/lib/queries/useUserQuery';
-import Image from 'next/image';
 import UserProfileEdit from './profile/UserProfileEdit';
 import Text from '../common/Text';
-import { getUserImageByLevel } from '@/lib/utils/user.utils';
 import UserProfileImage from '../common/UserProfileImage';
 
 const SidebarProfile = () => {
@@ -19,7 +17,6 @@ const SidebarProfile = () => {
   const totalPoints =
     profileData?.userPoints?.reduce((sum, p) => sum + p.points, 0) || 0;
   const level = Math.floor(totalPoints / 20) + 1;
-  const userImageSrc = getUserImageByLevel(level);
 
   return (
     <div className="my-[10px] flex flex-col items-center gap-1 md:gap-3">
