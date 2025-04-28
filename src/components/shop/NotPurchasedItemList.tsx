7514fb860bcb2a511576235cd56b0c04a7d4f77b
Handsol <imhandsol@gmail.com>
2025-04-28 15:55:37 +0900

    Feat: NotPurchasedItemList 컴포넌트에서 아이템 리스트를 쿼리로 가져오도록 수정 #177

--- src/components/shop/NotPurchasedItemList.tsx
+++ src/components/shop/NotPurchasedItemList.tsx
@@ -1,21 +1,29 @@
-import { ItemList } from '@/types/shop.type';
+import { ItemList, ShopItem } from '@/types/shop.type';
 import Title from '../common/Title';
 import { TITLE_MODE } from '@/constants/mode.constants';
 import NotPurchasedItemCard from './NotPurchasedItemCard';
 import { shopItemListUlStyle } from '@/styles/shopItemListStyles';
+import { useQuery } from '@tanstack/react-query';
+import { fetchGetNotPurchasedItemList } from '@/lib/services/payment-actions.services';
 
+// props로 받던 itemList는 useQuery를 사용해 가져올거라 제거
 type NotPurchasedItemListProps = {
-  itemList: ItemList;
   userId: string;
   userEmail: string | null | undefined;
 };
 
 const NotPurchasedItemList = ({
-  itemList,
   userId,
   userEmail,
 }: NotPurchasedItemListProps) => {
-  const itemNumber = itemList.length;
+  // tanstack query를 사용해 구매하지 않은 아이템 리스트를 가져오기
+  const { data: itemList } = useQuery({
+    queryKey: ['notPurchasedItems', userId],
+    queryFn: () => fetchGetNotPurchasedItemList(userId) as Promise<ShopItem[]>,
+  });
+
+  // 상품을 전체 구매해서 남은 상품이 없을 경우 빈 배열로 표기
+  const itemNumber = itemList?.length || 0;
 
   return (
     <article>
@@ -26,7 +34,7 @@ const NotPurchasedItemList = ({
         {`All Items (${itemNumber})`}
       </Title>
       <ul className={shopItemListUlStyle}>
-        {itemList.map((item) => (
+        {itemList?.map((item: ShopItem) => (
           <NotPurchasedItemCard
             item={item}
             userId={userId}
