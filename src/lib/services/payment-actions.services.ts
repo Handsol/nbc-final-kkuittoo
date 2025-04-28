36159553203ea7868d25deef89d4fdf2fc7efe47
Handsol <imhandsol@gmail.com>
2025-04-28 15:45:58 +0900

    Feat: 구매하지 않은 아이템 리스트를 가져오는 함수 추가 #177

--- src/lib/services/payment-actions.services.ts
+++ src/lib/services/payment-actions.services.ts
@@ -1,9 +1,20 @@
 import { SHOP_MESSAGE } from '@/constants/error-messages.constants';
 import { prisma } from '@/lib/prisma';
 
+// 구매하지 않은 아이템 리스트 가져오기
+export const fetchGetNotPurchasedItemList = async (userId: string) => {
+  try {
+    const { notPurchasedItemList } = await fetchGetItemList(userId);
+    return notPurchasedItemList;
+  } catch (error) {
+    console.error(error);
+    throw new Error(`Failed to fetch not purchased items: ${error}`);
+  }
+};
+
 export const fetchGetItemList = async (userId: string) => {
   try {
-    //아이템 전체 리스트와 해당 유저가 구매한 아이템 리스트 가져오기
+    // 아이템 전체 리스트와 해당 유저가 구매한 아이템 리스트 가져오기
     const itemList = await prisma.item.findMany({
       include: {
         userItems: {
