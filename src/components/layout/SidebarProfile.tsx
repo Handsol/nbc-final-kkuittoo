135fd72b4ac14148eb618fee1bc909cb4dad4860
miing_gaeng <pem4134@naver.com>
2025-04-29 12:08:43 +0900

    Fix: 사이드바 TQ queryFn 수정 및 프로필 공통 컴포넌트 적용 #187

--- src/components/layout/SidebarProfile.tsx
+++ src/components/layout/SidebarProfile.tsx
@@ -5,6 +5,7 @@ import { useUserQuery } from '@/lib/queries/useUserQuery';
 import UserProfileEdit from './profile/UserProfileEdit';
 import Text from '../common/Text';
 import UserProfileImage from '../common/UserProfileImage';
+import { UserPoint } from '@prisma/client';
 
 const SidebarProfile = () => {
   const { data: session } = useSession();
@@ -13,19 +14,24 @@ const SidebarProfile = () => {
   const { data: profileData, isPending } = useUserQuery(userId || '');
 
   if (isPending) return <Text>로딩 중...</Text>;
+  if (!profileData) throw new Error('유저 프로필 데이터 가져오기 실패');
 
   const totalPoints =
-    profileData?.userPoints?.reduce((sum, p) => sum + p.points, 0) || 0;
+    profileData.userPoints.reduce(
+      (sum: number, p: UserPoint) => sum + p.points,
+      0,
+    ) || 0;
   const level = Math.floor(totalPoints / 20) + 1;
+  const userItems = profileData.userItems;
 
   return (
     <div className="my-[10px] flex flex-col items-center gap-1 md:gap-3">
-      <UserProfileImage level={level} size="lg" />
+      <UserProfileImage level={level} size="lg" items={userItems} />
 
       <UserProfileEdit
-        name={profileData?.name || ''}
-        bio={profileData?.bio || ''}
-        userId={profileData?.id || ''}
+        name={profileData.name}
+        bio={profileData.bio}
+        userId={profileData.id}
       />
     </div>
   );
