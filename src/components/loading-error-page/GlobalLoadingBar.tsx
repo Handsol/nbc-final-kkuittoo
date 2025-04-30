5cb3746ad098c970dd67b8ea0285c42eb0b4b922
verdantgreeny <verdantgreeny@gmail.com>
2025-04-30 23:24:49 +0900

    Fix: 동적변수 제거 #206

--- src/components/loading-error-page/GlobalLoadingBar.tsx
+++ src/components/loading-error-page/GlobalLoadingBar.tsx
@@ -36,8 +36,11 @@ const GlobalLoadingBar = () => {
   if (!loading) return null;
 
   return (
+    // z-[9999]로 한 이유 : 상수처리한 동적변수는 Tailwind가 빌드 시점에 값을 알 수 없기 때문에 안되고
+    // 만약 쓸려면 인라인 스타일로 써야 한다고 합니다.
+    // 일단 저희 로그인하고 로딩화면 뜰 때 너무 이상하기 때문에 이부분 나중에 해결하는 게 좋을 거 같습니다.
     <div
-      className={`fixed inset-0 z-[${Z_INDEX.LOADING_BAR}] flex flex-col items-center justify-center bg-white transition-opacity duration-300`}
+      className={`fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-white transition-opacity duration-300`}
     >
       <Image
         src={IMAGE_ASSETS.LOGO.DESKTOP}
