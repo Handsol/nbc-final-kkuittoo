2b38c48d4ed9c2525acc2110762a86cc286495a1
bonseung-dev <glynnregister@gmail.com>
2025-04-30 23:46:14 +0900

    Hotfix: 아바타 안나오는 오류 수정 #207

--- src/components/common/UserProfileImage.tsx
+++ src/components/common/UserProfileImage.tsx
@@ -4,12 +4,9 @@ import { getUserImageByLevel } from '@/lib/utils/user.utils';
 import { CardUserItemData } from '@/types/rank.type';
 import Image from 'next/image';
 import clsx from 'clsx';
-import {
-  CONTAINER_SIZE_CLASSES,
-  IMAGE_SIZE_CLASSES,
-  type ProfileImageSize,
-} from '@/constants/image.constants';
 import { Z_INDEX } from '@/constants/z-index.constants';
+
+type ProfileImageSize = 'topRank' | 'normalRank' | 'member' | 'sidebar';
 
 type UserProfileImageProps = {
   level: number;
@@ -24,40 +21,44 @@ const UserProfileImage = ({
 }: UserProfileImageProps) => {
   const userImageSrc = getUserImageByLevel(level);
 
-  // 현재 적용 중인 아이템
   let currentAppliedItem: string | null = null;
-
   if (items && items.length > 0) {
     const appliedItem = items.find((item) => item.isApplied);
-
     if (appliedItem) {
       currentAppliedItem = appliedItem.item.itemImage;
     }
   }
 
+  const containerClass = clsx('relative flex items-center justify-center', {
+    'w-[100px] h-[100px] md:w-[160px] md:h-[160px]': size === 'topRank',
+    'w-[100px] h-[100px] md:w-[100px] md:h-[100px]': size === 'normalRank',
+    'w-[120px] h-[120px] md:w-[110px] md:h-[110px]': size === 'member',
+    'w-[120px] h-[120px] md:w-[150px] md:h-[150px]': size === 'sidebar',
+  });
+
+  const imageWrapperClass = clsx(
+    'relative z-[30] rounded-full overflow-hidden',
+    {
+      'w-[90px] h-[90px] md:w-[150px] md:h-[150px]': size === 'topRank',
+      'w-[90px] h-[90px] md:w-[90px] md:h-[90px]': size === 'normalRank',
+      'w-[120px] h-[120px] md:w-[110px] md:h-[110px]': size === 'member',
+      'w-[110px] h-[110px] md:w-[150px] md:h-[150px]': size === 'sidebar',
+    },
+  );
+
   return (
-    <div
-      className={clsx(
-        'relative flex items-center justify-center',
-        CONTAINER_SIZE_CLASSES[size],
-      )}
-    >
+    <div className={containerClass}>
       {/* 테두리 */}
       {currentAppliedItem && (
         <Image
           src={currentAppliedItem}
           alt="border"
           fill
-          className={`absolute rounded-full object-cover z-${Z_INDEX.RANK_LABEL} `}
+          className={`absolute z-${Z_INDEX.RANK_LABEL} rounded-full object-cover`}
         />
       )}
       {/* 프로필 캐릭터 */}
-      <div
-        className={clsx(
-          'absolute rounded-full overflow-hidden',
-          IMAGE_SIZE_CLASSES[size],
-        )}
-      >
+      <div className={imageWrapperClass}>
         <Image
           src={userImageSrc}
           alt="user profile"
